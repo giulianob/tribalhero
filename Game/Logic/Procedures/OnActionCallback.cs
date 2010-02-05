32613921333cc5b920236992cdeec75072a89c54
gbarberi <gbarberi@localhost>
2010-02-05 01:29:08 +0000

    Resource rate is reset after structure is downgraded. Fixes #75.

--- Game/Logic/Procedures/OnActionCallback.cs
+++ Game/Logic/Procedures/OnActionCallback.cs
@@ -7,17 +7,17 @@ using Game.Setup;
 
 namespace Game.Logic.Procedures {
     public partial class Procedure {
-        internal static void OnLaborUpdate(Structure structure, int laborDelta) {
+        public static void AdjustCityResourceRates(Structure structure, int laborDelta) {
             if (ObjectTypeFactory.IsStructureType("Wood", structure)) {
                 structure.City.Resource.Wood.Rate += laborDelta;
             }
 
             if (ObjectTypeFactory.IsStructureType("Crop", structure)) {
                 structure.City.Resource.Wood.Rate += laborDelta;
-            }
+            }            
         }
 
-        internal static void OnStructureUpgrade(Structure structure) {
+        public static void OnStructureUpgrade(Structure structure) {
             structure.City.BeginUpdate();
             Formula.ResourceCap(structure.City);
             structure.City.EndUpdate();

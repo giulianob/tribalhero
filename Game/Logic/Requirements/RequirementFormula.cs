8e308b64a558ae8114adfa548132d73fb7bacc41
gbarberi <gbarberi@localhost>
2010-02-15 01:53:03 +0000

    Optimized effect reqs

--- Game/Logic/Requirements/RequirementFormula.cs
+++ Game/Logic/Requirements/RequirementFormula.cs
@@ -30,45 +30,42 @@ namespace Game.Logic {
             return count >= int.Parse(parms[2]) ? Error.OK : Error.EFFECT_REQUIREMENT_NOT_MET;
         }
 
-        public static Error HaveStructure(GameObject obj, IEnumerable<Effect> effects, string[] parms) {
-            Structure structure;
+        public static Error HaveStructure(GameObject obj, IEnumerable<Effect> effects, string[] parms) {            
             ushort type = ushort.Parse(parms[0]);
             byte min = byte.Parse(parms[1]);
             byte max = byte.Parse(parms[2]);
 
-            foreach (GameObject tmp in obj.City) {
-                if ((structure = tmp as Structure) != null) {
-                    if (structure.Type == type && structure.Lvl >= min && structure.Lvl <= max)
-                        return Error.OK;
-                }
+            foreach (Structure structure in obj.City) {
+                if (structure.Type == type && structure.Lvl >= min && structure.Lvl <= max)
+                    return Error.OK;
             }
             return Error.EFFECT_REQUIREMENT_NOT_MET;
         }
 
         public static Error HaveNoStructure(GameObject obj, IEnumerable<Effect> effects, string[] parms) {
-            Structure structure;
             ushort type = ushort.Parse(parms[0]);
             byte min = byte.Parse(parms[1]);
             byte max = byte.Parse(parms[2]);
 
-            foreach (GameObject tmp in obj.City) {
-                if ((structure = tmp as Structure) != null) {
-                    if (structure.Type == type && structure.Lvl >= min && structure.Lvl <= max)
-                        return Error.EFFECT_REQUIREMENT_NOT_MET;
-                }
+            foreach (Structure structure in obj.City) {
+                if (structure.Type == type && structure.Lvl >= min && structure.Lvl <= max)
+                    return Error.EFFECT_REQUIREMENT_NOT_MET;
             }
+
             return Error.OK;
         }
 
         public static Error CountLessThan(GameObject obj, IEnumerable<Effect> effects, string[] parms) {
+            int effectCode = int.Parse(parms[0]);
+            int maxCount = int.Parse(parms[1]);
+
             int count = 0;
             foreach (Effect effect in effects) {
-                if (effect.id == EffectCode.CountEffect && (int) effect.value[0] == int.Parse(parms[0]))
+                if (effect.id == EffectCode.CountEffect && (int) effect.value[0] == effectCode)
                     count += (int) effect.value[1];
             }
-            if (count < int.Parse(parms[1]))
-                return Error.OK;
-            return Error.EFFECT_REQUIREMENT_NOT_MET;
+
+            return count < maxCount ? Error.OK : Error.EFFECT_REQUIREMENT_NOT_MET;
         }
     }
 }

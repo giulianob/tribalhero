991e9ca6e0075512cf97ea23d527685ab8241f57
Giuliano Barberi <gbarberi@aotaonline.com>
2010-05-09 17:52:34 -0400

    Merge

--- Game/Logic/Formulas/Cost.cs
+++ Game/Logic/Formulas/Cost.cs
@@ -3,7 +3,9 @@
 using System;
 using Game.Data;
 using Game.Setup;
-
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 #endregion
 
 namespace Game.Logic {
@@ -37,6 +39,26 @@ namespace Game.Logic {
         internal static int GetLaborRate(int laborTotal) {
             if (laborTotal < 140) laborTotal = 140;
             return (int)(86400 / (-6.845 * Math.Log(laborTotal) + 55));
+        }
+
+        internal static ushort LaborMoveMax(Structure structure) {
+            return (ushort)Math.Max(5, Math.Ceiling(structure.Stats.Base.MaxLabor / 10.0f));
+        }
+
+        internal static int GetAwayFromRadius(IEnumerable<Effect> effects, byte radius, ushort type) {
+            return radius + effects.Sum(x => (x.id == EffectCode.AwayFromStructureMod && (int)x.value[0] == type) ? (int)x.value[1] : 0);
+        }
+
+        static int[] rateCrop = { 0, 100, 200, 300, 500, 1000 };
+        static int[] rateWood = { 0, 100, 200, 300, 500, 1000 };
+        static int[] rateGold = { 0, 0, 50, 100, 200, 400 };
+        static int[] rateIron = { 0, 0, 0, 50, 100, 200 };
+        internal static Resource HiddenResource(City city) {
+            Resource resource = new Resource();
+            foreach (Structure structure in city.Where(x=>ObjectTypeFactory.IsStructureType("Basement",x))) {
+                resource.add(rateCrop[structure.Lvl], rateGold[structure.Lvl], rateIron[structure.Lvl], rateWood[structure.Lvl], 0);
+            }
+            return resource;
         }
     }
 }

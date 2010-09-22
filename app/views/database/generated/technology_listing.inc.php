8cf947d16bc45edda54cc2f675c4e0a2b73b09ee
Giuliano Barberi <gbarberi@aotaonline.com>
2010-09-21 21:46:50 -0400

    Completed first database implementation

---
+++ app/views/database/generated/technology_listing.inc.php
@@ -0,0 +1,22 @@
+<?php
+                    $technologies = array(
+                'ARCHERY_RESEARCH_TECHNOLOGY' => array('name' => 'Archery Research'),
+'HANZO_SWORD_TECHNOLOGY' => array('name' => 'Hanzo Sword'),
+'BOW_RESEARCH_TECHNOLOGY' => array('name' => 'New Bow Research'),
+'GROUND_ARMOR_RESEARCH_TECHNOLOGY' => array('name' => 'Ground Unit Defence'),
+'MOUNT_ARMOR_RESEARCH_TECHNOLOGY' => array('name' => 'Mounted Unit Defence'),
+'MACHINE_ARMOR_RESEARCH_TECHNOLOGY' => array('name' => 'Machinery Unit Defence'),
+'STRUCTURE_ARMOR_RESEARCH_TECHNOLOGY' => array('name' => 'Structure Defence'),
+'SWORD_WEAPON_RESEARCH_TECHNOLOGY' => array('name' => 'Sword Damage Boost'),
+'PIKE_WEAPON_RESEARCH_TECHNOLOGY' => array('name' => 'Pike Damage Boost'),
+'BOW_WEAPON_RESEARCH_TECHNOLOGY' => array('name' => 'Bow Damage Boost'),
+'BALL_WEAPON_RESEARCH_TECHNOLOGY' => array('name' => 'Ballistic Damage Boost'),
+'DOUBLE_TIME_TECHNOLOGY' => array('name' => 'Double Time'),
+'A_CALL_TO_ARMS_TECHNOLOGY' => array('name' => 'A Call To Arms'),
+'TUNNEL_EXCAVATION_TECHNOLOGY' => array('name' => 'Tunnel Excavation Research'),
+'ATTIC_STORAGE_TECHNOLOGY' => array('name' => 'Attic Storage'),
+'TWO_FOR_ONE_TECHNOLOGY' => array('name' => 'Two For One'),
+'FOUR_FOR_ONE_TECHNOLOGY' => array('name' => 'Four For One'),
+'EIGHT_FOR_ONE_TECHNOLOGY' => array('name' => 'Eight For One'),
+'HAPPYHOUR_TECHNOLOGY' => array('name' => 'Happy Hour'),
+);

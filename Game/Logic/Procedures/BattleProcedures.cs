f7444d3752930020b92549ad049096b537499d92
anthony <anthony@localhost>
2011-01-21 15:59:48 -0500

    Technology SenseOfUrgency added.

--- Game/Logic/Procedures/BattleProcedures.cs
+++ Game/Logic/Procedures/BattleProcedures.cs
@@ -5,7 +5,7 @@ using Game.Battle;
 using Game.Data;
 using Game.Data.Troop;
 using Game.Fighting;
-
+using System.Linq;
 #endregion
 
 namespace Game.Logic.Procedures {
@@ -26,6 +26,15 @@ namespace Game.Logic.Procedures {
             bm.AddToLocal(list, state);
             MoveUnitFormation(city.DefaultTroop, FormationType.NORMAL, FormationType.IN_BATTLE);
             city.DefaultTroop.EndUpdate(); 
+        }
+
+        internal static void SenseOfUrgency(City city, uint maxHP ) {
+            ushort restore = (ushort)(maxHP * (city.Technologies.GetEffects(EffectCode.CountEffect, EffectInheritance.ALL).Sum(x => (int)x.value[0] == 21103 ? (int)x.value[1] : 0) + 100) / 100);
+            foreach (Structure structure in city) {
+                structure.BeginUpdate();
+                structure.Stats.Hp += restore;
+                structure.EndUpdate();
+            }
         }
     }
 }

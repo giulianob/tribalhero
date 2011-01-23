fcaab6f0155cbd7e299f33813d5f41338d8eca0c
anthony <anthony@localhost>
2011-01-23 15:32:54 -0500

    Technology SenseOfUrgency fix: the formula was adding 120% of the destroyed HP, it should be 20%.

--- Game/Logic/Procedures/BattleProcedures.cs
+++ Game/Logic/Procedures/BattleProcedures.cs
@@ -29,7 +29,7 @@ namespace Game.Logic.Procedures {
         }
 
         internal static void SenseOfUrgency(City city, uint maxHP ) {
-            ushort restore = (ushort)(maxHP * (city.Technologies.GetEffects(EffectCode.CountEffect, EffectInheritance.ALL).Sum(x => (int)x.value[0] == 21103 ? (int)x.value[1] : 0) + 100) / 100);
+            ushort restore = (ushort)(maxHP * city.Technologies.GetEffects(EffectCode.CountEffect, EffectInheritance.ALL).Sum(x => (int)x.value[0] == 21103 ? (int)x.value[1] : 0) / 100);
             foreach (Structure structure in city) {
                 structure.BeginUpdate();
                 structure.Stats.Hp += restore;

04af08a306829f9263cf1920258744ad735745b9
Giuliano Barberi <gbarberi@aotaonline.com>
2011-03-17 19:52:35 -0400

    Defaulting the city value to 1 when upgrading db

--- app/config/sql/migrate/20110317223328_AddCityValue.php
+++ app/config/sql/migrate/20110317223328_AddCityValue.php
@@ -2,11 +2,12 @@
 
 class AddCityValue extends Ruckusing_BaseMigration {
 
-	public function up() {
-		$this->add_column('cities', 'value', 'smallinteger', array('unsigned' => true, 'null' => false));
-	}
+    public function up() {
+        $this->add_column('cities', 'value', 'smallinteger', array('unsigned' => true, 'null' => false, 'default' => 1));
+    }
 
-	public function down() {
-            $this->remove_column('cities', 'value');
-	}
+    public function down() {
+        $this->remove_column('cities', 'value');
+    }
+
 }

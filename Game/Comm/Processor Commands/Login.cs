0bad21520b772be39c861c45eba26936564085e6
anthony <anthony@localhost>
2011-03-29 19:11:41 -0400

    Building's upkeep for calculating MissChance is divided by 5. Reduce overall building's strength ("equivalent upkeep")

---
+++ Game/Comm/Processor Commands/Login.cs
@@ -0,0 +1,295 @@
+#region
+
+using System;
+using System.Data;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Game.Data;
+using Game.Data.Troop;
+using Game.Database;
+using Game.Logic;
+using Game.Logic.Actions;
+using Game.Logic.Formulas;
+using Game.Setup;
+using Game.Util;
+
+#endregion
+
+namespace Game.Comm
+{
+    public partial class Processor
+    {
+        private readonly object loginLock = new object();
+
+        public void CmdQueryXml(Session session, Packet packet)
+        {
+            var reply = new Packet(packet);
+            reply.AddString(File.ReadAllText(Path.Combine(Config.data_folder, "data.xml")));
+            session.Write(reply);
+        }
+
+        public void CmdLogin(Session session, Packet packet)
+        {
+            Player player;
+            var reply = new Packet(packet);
+            reply.Option |= (ushort)Packet.Options.Compressed;
+
+            short clientVersion;
+            short clientRevision;
+            byte loginMode;
+            string loginKey = string.Empty;
+            string playerName = string.Empty;
+            string playerPassword = string.Empty;
+            uint playerId;
+            bool admin = false;
+            bool banned = false;
+
+            try
+            {
+                clientVersion = packet.GetInt16();
+                clientRevision = packet.GetInt16();
+                loginMode = packet.GetByte();
+                playerName = packet.GetString();
+                if (loginMode == 0)                
+                    loginKey = packet.GetString();                
+                else
+                    playerPassword = packet.GetString();
+            }
+            catch(Exception)
+            {
+                ReplyError(session, packet, Error.Unexpected);
+                session.CloseSession();
+                return;
+            }
+
+            if (clientVersion < Config.client_min_version || clientRevision < Config.client_min_revision)
+            {
+                ReplyError(session, packet, Error.ClientOldVersion);
+                session.CloseSession();
+            }
+
+            if (Config.database_load_players)
+            {
+                ApiResponse response;
+                try
+                {
+                    if (loginMode == 0)                    
+                        response = ApiCaller.CheckLoginKey(playerName, loginKey);                    
+                    else                    
+                        response = ApiCaller.CheckLogin(playerName, playerPassword);                    
+                }
+                catch(Exception e)
+                {
+                    Global.Logger.Error("Error loading player", e);
+                    ReplyError(session, packet, Error.Unexpected);
+                    session.CloseSession();
+                    return;
+                }
+
+                if (!response.Success)
+                {
+                    ReplyError(session, packet, Error.InvalidLogin);
+                    session.CloseSession();
+                    return;
+                }
+
+                playerId = uint.Parse(response.Data.player.id);
+                playerName = response.Data.player.name;
+                banned = int.Parse(response.Data.player.banned) == 1;
+                admin = int.Parse(response.Data.player.admin) == 1;
+
+                // If we are under admin only mode then kick out non admin
+                if (Config.server_admin_only && !admin)
+                {
+                    ReplyError(session, packet, Error.UnderMaintenance);
+                    session.CloseSession();
+                    return;
+                }
+
+                // If player was banned then kick his ass out
+                if (banned)
+                {
+                    ReplyError(session, packet, Error.Banned);
+                    session.CloseSession();
+                    return;
+                }
+
+            }
+            else
+            {
+                if (!uint.TryParse(playerName, out playerId))
+                {
+                    ReplyError(session, packet, Error.PlayerNotFound);
+                    session.CloseSession();
+                    return;
+                }
+
+                playerName = "Player " + playerId;
+            }
+
+            //Create the session id that will be used for the calls to the web server                        
+#if DEBUG
+            string sessionId = playerId.ToString();
+            admin = true;
+#else
+            SHA1 sha = new SHA1CryptoServiceProvider();
+
+            string sessionId = BitConverter.ToString(
+                sha.ComputeHash(Encoding.UTF8.GetBytes(playerId + Config.database_salt + DateTime.UtcNow.Ticks))).
+                Replace("-", String.Empty);
+#endif
+
+            bool newPlayer;
+            lock (loginLock)
+            {
+                newPlayer = !Global.World.Players.TryGetValue(playerId, out player);
+
+                //If it's a new player then add him to our session
+                if (newPlayer)
+                {
+                    Global.Logger.Info(string.Format("Creating new player {0}({1})", playerName, playerId));
+
+                    player = new Player(playerId, SystemClock.Now, SystemClock.Now, playerName, admin, sessionId);
+
+                    Global.World.Players.Add(player.PlayerId, player);
+                }
+                else
+                {
+                    Global.Logger.Info(string.Format("Player login in {0}({1})", player.Name, player.PlayerId));
+
+                    Global.DbManager.Query(string.Format("UPDATE `{0}` SET `session_id` = '{1}' WHERE `id` = '{2}' LIMIT 1", Player.DB_TABLE, sessionId, playerId), new DbColumn[] {});
+                    player.SessionId = sessionId;
+                    player.Admin = admin;
+                    player.LastLogin = SystemClock.Now;
+                }
+            }
+
+            using (new MultiObjectLock(player))
+            {
+                if (!newPlayer)
+                {
+                    // If someone is already connected as this player, kick them off
+                    if (player.Session != null)
+                    {
+                        player.Session.CloseSession();
+                        player.Session = null;
+                    }
+
+                    player.Session = session;
+                    Global.DbManager.Save(player);
+                }
+                else
+                {
+                    Global.DbManager.Save(player);
+                }
+
+                //User session
+                session.Player = player;
+                player.Session = session;
+
+                //Player Id
+                reply.AddUInt32(player.PlayerId);
+                reply.AddByte((byte)(player.Admin ? 1 : 0));
+                reply.AddString(sessionId);
+                reply.AddString(player.Name);
+
+                //Server time
+                reply.AddUInt32(UnixDateTime.DateTimeToUnix(DateTime.UtcNow.ToUniversalTime()));
+
+                //Server rate
+                reply.AddString(Config.seconds_per_unit.ToString());
+
+                // If it's a new player we send simply a 1 which means the client will need to send back a city name
+                // Otherwise, we just send the whole login info
+                if (player.GetCityList().Count == 0)
+                    reply.AddByte(1);
+                else
+                {
+                    reply.AddByte(0);
+                    PacketHelper.AddLoginToPacket(session, reply);
+                }
+
+                session.Write(reply);
+
+                //Restart any city actions that may have been stopped due to inactivity
+                foreach (var city in
+                        player.GetCityList().Where(city => !city.Worker.PassiveActions.Exists(x => x.Type == ActionType.CityPassive)))
+                    city.Worker.DoPassive(city, new CityPassiveAction(city.Id), false);
+            }
+        }
+
+        public void CmdCreateInitialCity(Session session, Packet packet)
+        {
+            using (new MultiObjectLock(session.Player))
+            {
+                string cityName;
+                try
+                {
+                    cityName = packet.GetString().Trim();
+                }
+                catch(Exception)
+                {
+                    ReplyError(session, packet, Error.Unexpected);
+                    return;
+                }
+
+                // Verify city name is valid
+                if (!City.IsNameValid(cityName))
+                {
+                    ReplyError(session, packet, Error.CityNameInvalid);
+                    return;
+                }
+
+                City city;
+                Structure mainBuilding;
+
+                lock (Global.World.Lock)
+                {
+                    // Verify city name is unique
+                    if (Global.World.CityNameTaken(cityName))
+                    {
+                        ReplyError(session, packet, Error.CityNameTaken);
+                        return;
+                    }
+
+                    if (!Randomizer.MainBuilding(out mainBuilding, Formula.GetInitialCityRadius(), 1))
+                    {
+                        Global.World.Players.Remove(session.Player.PlayerId);
+                        Global.DbManager.Rollback();
+                        // If this happens I'll be a very happy game developer
+                        ReplyError(session, packet, Error.MapFull);
+                        return;
+                    }
+
+                    city = new City(session.Player, cityName, Formula.GetInitialCityResources(), Formula.GetInitialCityRadius(), mainBuilding);
+                    session.Player.Add(city);
+
+                    Global.World.Add(city);
+                    mainBuilding.BeginUpdate();
+                    Global.World.Add(mainBuilding);
+                    mainBuilding.EndUpdate();
+
+                    var defaultTroop = new TroopStub();
+                    defaultTroop.BeginUpdate();
+                    defaultTroop.AddFormation(FormationType.Normal);
+                    defaultTroop.AddFormation(FormationType.Garrison);
+                    defaultTroop.AddFormation(FormationType.InBattle);
+                    city.Troops.Add(defaultTroop);
+                    defaultTroop.EndUpdate();
+                }
+
+                InitFactory.InitGameObject(InitCondition.OnInit, mainBuilding, mainBuilding.Type, mainBuilding.Stats.Base.Lvl);
+
+                city.Worker.DoPassive(city, new CityPassiveAction(city.Id), false);
+
+                var reply = new Packet(packet);
+                reply.Option |= (ushort)Packet.Options.Compressed;
+                PacketHelper.AddLoginToPacket(session, reply);
+                session.Write(reply);
+            }
+        }
+    }
+}
---
+++ Game/Logic/Randomizer.cs
@@ -0,0 +1,91 @@
+#region
+
+using System;
+using Game.Data;
+using Game.Database;
+using Game.Map;
+using Game.Setup;
+
+#endregion
+
+namespace Game.Logic
+{
+    public class Randomizer
+    {
+        public static Random Random = Config.Random;
+
+        public static uint MapX
+        {
+            get
+            {
+                return (uint)Random.Next(Config.width_margin, 50 - Config.width_margin);
+            }
+        }
+
+        public static uint MapY
+        {
+            get
+            {
+                return (uint)Random.Next(Config.height_margin, 50 - Config.height_margin);
+            }
+        }
+
+        public static bool MainBuilding(out Structure structure, byte radius, byte lvl)
+        {
+            structure = StructureFactory.GetNewStructure(2000, lvl);
+            uint x, y;
+            if (!MapFactory.NextLocation(out x, out y, radius))
+            {
+                structure = null;
+                return false;
+            }
+            structure.X = x;
+            structure.Y = y;
+            return true;
+        }
+
+        private static bool RandomizeNpcResourceWork(uint ox, uint oy, uint x, uint y, object custom)
+        {
+            var feObj = (RandomForeach)custom;
+            if (Config.Random.Next()%4 == 0)
+            {
+                Structure structure;
+                Global.World.LockRegion(x, y);
+                if (Config.Random.Next()%2 == 0)
+                    structure = StructureFactory.GetNewStructure(2402, 1);
+                else
+                    structure = StructureFactory.GetNewStructure(2402, 1);
+                structure.X = x;
+                structure.Y = y;
+                feObj.City.Add(structure);
+                if (!Global.World.Add(structure))
+                    feObj.City.ScheduleRemove(structure, false);
+                InitFactory.InitGameObject(InitCondition.OnInit, structure, structure.Type, structure.Lvl);
+                Global.DbManager.Save(structure);
+                Global.World.UnlockRegion(x, y);
+            }
+            return true;
+        }
+
+        public static void RandomizeNpcResource(City city, DbTransaction transaction)
+        {
+            byte radius = city.Radius;
+            var feObject = new RandomForeach(city);
+            TileLocator.ForeachObject(city.X, city.Y, (byte)Math.Max(radius - 1, 0), false, RandomizeNpcResourceWork, feObject);
+        }
+
+        #region Nested type: RandomForeach
+
+        private class RandomForeach
+        {
+            public RandomForeach(City city)
+            {
+                City = city;
+            }
+
+            public City City { get; private set; }
+        }
+
+        #endregion
+    }
+}

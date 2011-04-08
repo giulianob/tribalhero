c0b88c15bd12a1f49bd3e3bc82d4eb0af9497a05
Giuliano Barberi <gbarberi@aotaonline.com>
2011-04-08 16:10:12 -0400

    First rev of building new city. Broken currently

---
+++ Game/Battle/BattleOrder.cs
@@ -0,0 +1,37 @@
+#region
+
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace Game.Battle
+{
+    class BattleOrder : List<CombatObject>
+    {
+        private uint round;
+
+        public BattleOrder(uint round)
+        {
+            this.round = round;
+        }
+
+        public void ParticipatedInRound()
+        {
+            round++;
+        }
+
+        public bool NextObject(out CombatObject outObj)
+        {
+            if (Count == 0)
+            {
+                outObj = null;
+                return true;
+            }
+
+            outObj = this.FirstOrDefault(obj => obj.LastRound == round) ?? this.FirstOrDefault(obj => obj.LastRound == round + 1);
+
+            return !TrueForAll(t => t.LastRound == round + 1);
+        }
+    }
+}
---
+++ Game/Comm/Parameter.cs
@@ -0,0 +1,81 @@
+#region
+
+using System;
+using System.Text;
+
+#endregion
+
+namespace Game.Comm
+{
+    public class Parameter
+    {
+        private readonly byte[] bytes;
+
+        public Parameter(int value)
+        {
+            Value = value;
+            Length = sizeof(int);
+            bytes = BitConverter.GetBytes(value);
+        }
+
+        public Parameter(long value)
+        {
+            Value = value;
+            Length = sizeof(long);
+            bytes = BitConverter.GetBytes(value);
+        }
+
+        public Parameter(uint value)
+        {
+            Value = value;
+            Length = sizeof(uint);
+            bytes = BitConverter.GetBytes(value);
+        }
+
+        public Parameter(ushort value)
+        {
+            Value = value;
+            Length = sizeof(ushort);
+            bytes = BitConverter.GetBytes(value);
+        }
+
+        public Parameter(byte value)
+        {
+            Value = value;
+            Length = sizeof(byte);
+            bytes = new[] {value};
+        }
+
+        public Parameter(float value)
+        {
+            Value = value;
+            Length = sizeof(float);
+            bytes = BitConverter.GetBytes(value);
+        }
+
+        public Parameter(string value)
+        {
+            Value = value;
+            Length = sizeof(ushort) + value.Length;
+            bytes = new byte[2 + Length];
+            Buffer.BlockCopy(BitConverter.GetBytes((ushort)Length), 0, bytes, 0, 2);
+            Buffer.BlockCopy(Encoding.UTF8.GetBytes(value), 0, bytes, 2, value.Length);
+        }
+
+        public Parameter(byte[] value)
+        {
+            Value = value;
+            Length = value.Length;
+            bytes = value;
+        }
+
+        public object Value { get; private set; }
+
+        public int Length { get; private set; }
+
+        public byte[] GetBytes()
+        {
+            return bytes;
+        }
+    }
+}

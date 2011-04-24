1e04dde2f66dd6964424a272c37ff3c500c96362
Giuliano Barberi <gbarberi@aotaonline.com>
2011-04-24 16:49:46 -0400

    Fixed. Database empty shouldnt delete the schema_migrations table.

---
+++ Game/Comm/CmdLineCommands.cs
@@ -0,0 +1,10 @@
+﻿namespace Game.Comm
+{
+    public enum CmdLineCommand
+    {
+        SendResources = 100,
+        Ban = 110,
+        Unban = 111,
+        Delete = 112,
+    }
+}
---
+++ Game/Logic/Actions/ResourceActions/ForestCreatorAction.cs
@@ -0,0 +1,46 @@
+﻿#region
+
+using System;
+using Game.Data;
+using Game.Logic.Formulas;
+using Game.Setup;
+using Game.Util;
+
+#endregion
+
+namespace Game.Logic.Actions.ResourceActions
+{
+    public class ForestCreatorAction : ISchedule
+    {
+        public ForestCreatorAction()
+        {
+            Time = SystemClock.Now;
+        }
+
+        #region ISchedule Members
+
+        public DateTime Time { get; private set; }
+        public bool IsScheduled { get; set; }
+
+        public void Callback(object custom)
+        {
+            using (new MultiObjectLock(Global.World.Forests))
+            {
+                for (byte i = 0; i < Config.forest_count.Length; i++)
+                {
+                    var lvl = (byte)(i + 1);
+                    int delta = Config.forest_count[i] - Global.World.Forests.ForestCount[i];
+
+                    for (int j = 0; j < delta; j++)
+                        Global.World.Forests.CreateForest(lvl, Formula.GetMaxForestCapacity(lvl), Formula.GetMaxForestRate(lvl));
+                }
+
+                // Reschedule ourselves
+                Time = SystemClock.Now.AddMinutes(5);
+                Global.Scheduler.Put(this);
+            }
+        }
+
+        #endregion
+    }
+}

65b8a423bde1a52c9211841998992f7e4847a886
Giuliano Barberi <gbarberi@aotaonline.com>
2011-04-24 16:48:26 -0400

    Forcing cityId 0 in ranking for all the player related rankings

---
+++ app/app_controller.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @property EmailComponent $Email
+ * @property SessionComponent $Session
+ */
+class AppController extends Controller {
+
+    var $components = array('Auth', 'Security', 'Session', 'DebugKit.Toolbar');
+    var $helpers = array('Html', 'Js', 'Form', 'Session', 'Cache');
+
+    function beforeFilter() {
+        $this->disableCache();
+
+        $this->Auth->authError = "You have to login to do that!";
+        $this->Auth->userModel = 'Player';
+        $this->Auth->fields = array('username' => 'name', 'password' => 'password');
+        $this->Auth->loginAction = array('admin' => false, 'controller' => 'players', 'action' => 'login');
+        $this->Auth->loginRedirect = array('/');
+        $this->Auth->autoRedirect = false;
+        $this->Auth->authorize = 'controller';
+
+        if (isset($this->allowedFromGame) && in_array($this->action, $this->allowedFromGame)) {
+            if (array_key_exists('sessionId', $this->params['form']) && array_key_exists('playerId', $this->params['form']) && !empty($this->params['form']['sessionId']) && !empty($this->params['form']['playerId'])) {
+                $this->Auth->fields = array('username' => 'id', 'password' => 'session_id');
+
+                $playerModel = & ClassRegistry::init('Player');
+
+                if ($this->Auth->login(array('id' => $this->params['form']['playerId'], 'session_id' => $this->params['form']['sessionId']))) {
+                    $this->Auth->allow($this->action);
+                } else {
+                    $this->Auth->deny($this->action);
+                }
+            }
+        }
+    }
+
+    function isAuthorized() {
+        if ((array_key_exists('admin', $this->params) && $this->params['admin']) && !$this->Auth->user('admin')) {
+            return false;
+        }
+
+        if (isset($this->allowedFromGame) && in_array($this->action, $this->allowedFromGame)) {
+            if (!array_key_exists('sessionId', $this->params['form']) || !array_key_exists('playerId', $this->params['form'])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static function getCorrectMTime($filePath) {
+
+        $time = filemtime($filePath);
+
+        $isDST = (date('I', $time) == 1);
+        $systemDST = (date('I') == 1);
+
+        $adjustment = 0;
+
+        if ($isDST == false && $systemDST == true)
+            $adjustment = 3600;
+
+        else if ($isDST == true && $systemDST == false)
+            $adjustment = -3600;
+
+        else
+            $adjustment = 0;
+
+        return ($time + $adjustment);
+    }
+
+}
---
+++ app/models/player.php
@@ -0,0 +1,46 @@
+<?php
+
+class Player extends AppModel {
+
+    var $name = 'Player';
+    var $validate = array(
+        'name' => array(
+            array('rule' => 'notEmpty', 'required' => true, 'on' => 'create'),
+            array('rule' => array('between', 5, 32), 'message' => 'Name must be between 5 and 32 characters in length'),
+            array('rule' => 'alphaNumeric', 'message' => 'Player name must only contain letters and numbers'),
+            array('rule' => 'isUnique', 'message' => 'A player with this name already exists'),
+            array('rule' => '/^((?!system).)*$/i', 'message' => 'Illegal player name. Player names cannot contain the word "System"'),
+        ),
+        'email_address' => array(
+            array('rule' => 'notEmpty', 'required' => true, 'on' => 'create'),
+            array('rule' => 'isUnique', 'message' => 'This email address has already been registered'),
+            array('rule' => array('email', false), 'message' => 'This is not a valid email address'),
+        ),
+        'password_once' => array(
+            array('rule' => 'notEmpty', 'required' => true, 'on' => 'create'),
+            array('rule' => array('match', 'password_twice'), 'message' => 'Passwords must match'),
+            array('rule' => array('between', 5, 32), 'message' => 'Must be between 5 and 32 characters long'),
+        )
+    );
+    var $hasOne = array(
+        'City'
+    );
+    var $hasMany = array(
+        'SendMessage' => array(
+            'className' => 'Message',
+            'foreignKey' => 'sender_player_id',
+            'conditions' => array('Message.sender_state !=' => '2'),
+            'order' => 'Message.created DESC',
+            'limit' => '30',
+            'dependent' => true
+        ),
+        'ReceiveMessage' => array(
+            'className' => 'Message',
+            'foreignKey' => 'recipient_player_id',
+            'conditions' => array('Message.recipient_state !=' => '2'),
+            'order' => 'Message.created DESC',
+            'limit' => '30',
+            'dependent' => true
+        )
+    );
+}

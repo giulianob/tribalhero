7007517ede89f3748b66e2eca6b24f03b9b4cca0
Giuliano Barberi <gbarberi@aotaonline.com>
2011-05-01 16:15:19 -0400

    Added admin cmd to clear player description

---
+++ Game/Comm/Processor Commands/Battle.cs
@@ -0,0 +1,95 @@
+#region
+
+using System;
+using System.Collections.Generic;
+using Game.Data;
+using Game.Setup;
+using Game.Util;
+
+#endregion
+
+namespace Game.Comm
+{
+    public partial class Processor
+    {
+        public void CmdBattleSubscribe(Session session, Packet packet)
+        {
+            uint cityId;
+            City city;
+            try
+            {
+                cityId = packet.GetUInt32();
+            }
+            catch(Exception)
+            {
+                ReplyError(session, packet, Error.Unexpected);
+                return;
+            }
+
+            if (!Global.World.TryGetObjects(cityId, out city))
+                ReplyError(session, packet, Error.CityNotFound);
+
+            CallbackLock.CallbackLockHandler lockHandler = delegate
+                {
+                    var toBeLocked = new List<ILockable> {session.Player};
+
+                    if (city.Battle != null)
+                        toBeLocked.AddRange(city.Battle.LockList);
+
+                    return toBeLocked.ToArray();
+                };
+
+            using (new CallbackLock(lockHandler, null, city))
+            {
+                if (city.Battle == null)
+                {
+                    ReplyError(session, packet, Error.Unexpected);
+                    return;
+                }
+
+                int roundsLeft;
+                if (!Config.battle_instant_watch && !city.Battle.CanWatchBattle(session.Player, out roundsLeft))
+                {
+                    packet = ReplyError(session, packet, Error.BattleNotViewable, false);
+                    packet.AddInt32(roundsLeft);
+                    session.Write(packet);
+                    return;
+                }
+
+                var reply = new Packet(packet);
+                PacketHelper.AddToPacket(city.Battle.Attacker, reply);
+                PacketHelper.AddToPacket(city.Battle.Defender, reply);
+                city.Battle.Subscribe(session);
+                session.Write(reply);
+            }
+        }
+
+        public void CmdBattleUnsubscribe(Session session, Packet packet)
+        {
+            uint cityId;
+            City city;
+            try
+            {
+                cityId = packet.GetUInt32();
+            }
+            catch(Exception)
+            {
+                ReplyError(session, packet, Error.Unexpected);
+                return;
+            }
+
+            using (new MultiObjectLock(cityId, out city))
+            {
+                if (city == null || city.Battle == null)
+                {
+                    ReplyError(session, packet, Error.Unexpected);
+                    return;
+                }
+
+                city.Battle.Unsubscribe(session);
+            }
+
+            ReplySuccess(session, packet);
+        }
+    }
+}

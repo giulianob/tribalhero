f0a379e8dc5fbf7cf3f080c1f9dcfcba70114767
Giuliano Barberi <gbarberi@aotaonline.com>
2011-06-08 19:17:26 -0400

    Added auto complete functionality

---
+++ app/controllers/players_controller.php
@@ -0,0 +1,28 @@
+<?php
+
+class PlayersController extends AppController {
+
+    var $allowedFromGame = array('autocomplete');
+
+    function autocomplete() {
+        $data = array();
+
+        $name = $this->params['form']['name'];
+
+        if (empty($name) || strlen($name) < 3) {
+            $this->set(compact('data'));
+            $this->render('/elements/to_json');
+        }
+
+        $data = array_values($this->Player->find('list', array(
+                    'fields' => array('name'),
+                    'conditions' => array('name LIKE' => $name . '%'),
+                    'order' => 'LENGTH(name) ASC',
+                    'limit' => 6
+                )));
+
+        $this->set(compact('data'));
+        $this->render('/elements/to_json');
+    }
+
+}

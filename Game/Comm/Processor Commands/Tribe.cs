6a377777f2de2087d441afe6bb38387021eba2e9
anthony <anthony@localhost>
2011-08-28 16:40:44 -0400

    Updated blacksmith and armory technology.

---
+++ Game/Comm/Processor Commands/Tribe.cs
@@ -0,0 +1,291 @@
+#region
+
+using System;
+using System.Collections.Generic;
+using Game.Data;
+using Game.Data.Tribe;
+using Game.Logic;
+using Game.Logic.Actions;
+using Game.Logic.Formulas;
+using Game.Setup;
+using Game.Util;
+using System.Linq;
+#endregion
+
+namespace Game.Comm
+{
+    public partial class Processor
+    {
+
+        public void CmdTribeSetDescription(Session session, Packet packet)
+        {
+            string description;
+            try
+            {
+                description = packet.GetString();
+            }
+            catch (Exception)
+            {
+                ReplyError(session, packet, Error.Unexpected);
+                return;
+            }
+
+            using (new MultiObjectLock(session.Player))
+            {
+                if (session.Player.Tribesman == null)
+                {
+                    ReplyError(session, packet, Error.TribeIsNull);
+                    return;
+                }
+
+                if (!session.Player.Tribesman.Tribe.IsOwner(session.Player))
+                {
+                    ReplyError(session, packet, Error.Unexpected);
+                    return;
+                }
+
+                if (description.Length > Player.MAX_DESCRIPTION_LENGTH)
+                {
+                    ReplyError(session, packet, Error.Unexpected);
+                    return;
+                }
+
+                session.Player.Tribesman.Tribe.Description = description;
+
+                ReplySuccess(session, packet);
+            }
+        }
+
+        public void CmdTribeName(Session session, Packet packet)
+        {
+            var reply = new Packet(packet);
+
+            byte count;
+            uint[] tribeIds;
+            try
+            {
+                count = packet.GetByte();
+                tribeIds = new uint[count];
+                for (int i = 0; i < count; i++)
+                    tribeIds[i] = packet.GetUInt32();
+            }
+            catch (Exception)
+            {
+                ReplyError(session, packet, Error.Unexpected);
+                return;
+            }
+
+            reply.AddByte(count);
+            for (int i = 0; i < count; i++)
+            {
+                uint tribeId = tribeIds[i];
+                Tribe tribe;
+
+                if (!Global.Tribes.TryGetValue(tribeId, out tribe))
+                {
+                    ReplyError(session, packet, Error.Unexpected);
+                    return;
+                }
+
+                reply.AddUInt32(tribeId);
+                reply.AddString(tribe.Name);
+            }
+
+            session.Write(reply);
+        }
+
+        public void CmdTribeInfo(Session session, Packet packet)
+        {
+            var reply = new Packet(packet);
+            if (session.Player.Tribesman == null)
+            {
+                ReplyError(session, packet, Error.TribeIsNull);
+                return;
+            }
+            var tribe = session.Player.Tribesman.Tribe;
+
+            using (new MultiObjectLock(tribe))
+            {
+                reply.AddUInt32(tribe.Id);
+                reply.AddUInt32(tribe.Owner.PlayerId);
+                reply.AddByte(tribe.Level);
+                reply.AddString(tribe.Name);
+                reply.AddString(tribe.Description);
+                PacketHelper.AddToPacket(tribe.Resource, reply);
+
+                reply.AddInt16((short)tribe.Count);
+                foreach (var tribesman in tribe)
+                {
+                    reply.AddUInt32(tribesman.Player.PlayerId);
+                    reply.AddString(tribesman.Player.Name);
+                    reply.AddInt32(tribesman.Player.GetCityCount());
+                    reply.AddByte(tribesman.Rank);
+                    PacketHelper.AddToPacket(tribesman.Contribution, reply);
+                }
+
+                // Incoming List
+                var incomingList = tribe.GetIncomingList().ToList();
+                reply.AddInt16((short)incomingList.Count());
+                foreach (var incoming in incomingList)
+                {
+                    // Target
+                    City targetCity;
+                    if (Global.World.TryGetObjects(incoming.Action.To, out targetCity))
+                    {                        
+                        reply.AddUInt32(targetCity.Owner.PlayerId);
+                        reply.AddUInt32(targetCity.Id);
+                        reply.AddString(targetCity.Owner.Name);
+                        reply.AddString(targetCity.Name);
+                    }
+                    else
+                    {
+                        reply.AddUInt32(0);
+                        reply.AddUInt32(0);
+                        reply.AddString("N/A");
+                        reply.AddString("N/A");                        
+                    }
+
+                    // Attacker
+                    reply.AddUInt32(incoming.Action.WorkerObject.City.Owner.PlayerId);
+                    reply.AddUInt32(incoming.Action.WorkerObject.City.Id);
+                    reply.AddString(incoming.Action.WorkerObject.City.Owner.Name);
+                    reply.AddString(incoming.Action.WorkerObject.City.Name);
+
+                    reply.AddUInt32(UnixDateTime.DateTimeToUnix(incoming.Action.EndTime.ToUniversalTime()));
+                }
+
+                // Assignment List
+                reply.AddInt16(tribe.AssignmentCount);
+                foreach (var assignment in (IEnumerable<Assignment>)tribe)
+                {
+                    PacketHelper.AddToPacket(assignment, reply);
+                }
+
+                session.Write(reply);
+            }
+        }
+
+        public void CmdTribeCreate(Session session, Packet packet)
+        {
+            string name;
+            try
+            {
+                name = packet.GetString();
+            }
+            catch (Exception)
+            {
+                ReplyError(session, packet, Error.Unexpected);
+                return;
+            }
+
+            using (new MultiObjectLock(session.Player))
+            {
+                if (session.Player.Tribesman != null)
+                {
+                    ReplyError(session, packet, Error.TribesmanAlreadyInTribe);
+                    return;
+                }
+
+                if (Global.World.TribeNameTaken(name))
+                {
+                    ReplyError(session, packet, Error.TribeAlreadyExists);
+                    return;
+                }
+
+                if (!Tribe.IsNameValid(name))
+                {
+                    ReplyError(session, packet, Error.TribeNameInvalid);
+                    return;
+                }
+
+                if (session.Player.GetCityList().Count(city => city.Lvl >= 10) < 1)
+                {
+                    ReplyError(session, packet, Error.EffectRequirementNotMet);
+                    return;
+                }
+
+                Tribe tribe = new Tribe(session.Player, name);
+                Global.Tribes.Add(tribe.Id, tribe);
+                Global.DbManager.Save(tribe);
+
+                Tribesman tribesman = new Tribesman(tribe, session.Player, 0);
+                tribe.AddTribesman(tribesman);
+                ReplySuccess(session, packet);
+            }
+        }
+
+        public void CmdTribeDelete(Session session, Packet packet)
+        {
+            if (session.Player.Tribesman == null)
+            {
+                ReplyError(session, packet, Error.TribeIsNull);
+                return;
+            }
+
+            Tribe tribe = session.Player.Tribesman.Tribe;
+            using (new CallbackLock(custom => ((IEnumerable<Tribesman>)tribe).ToArray(), new object[] { }, tribe))
+            {
+                if (!session.Player.Tribesman.Tribe.IsOwner(session.Player))
+                {
+                    ReplyError(session, packet, Error.TribesmanNotAuthorized);
+                    return;
+                }
+
+                if( tribe.AssignmentCount>0 )
+                {
+                    ReplyError(session, packet, Error.TribeHasAssignment);
+                    return;                    
+                }
+
+                foreach (var tribesman in new List<Tribesman>(tribe))
+                    tribe.RemoveTribesman(tribesman.Player.PlayerId);
+                
+                Global.Tribes.Remove(tribe.Id);
+                Global.DbManager.Delete(tribe);
+            }
+
+            ReplySuccess(session, packet);
+        }
+
+        public void CmdTribeUpgrade(Session session, Packet packet)
+        {
+            if (!session.Player.Tribesman.Tribe.IsOwner(session.Player))
+            {
+                ReplyError(session, packet, Error.TribesmanNotAuthorized);
+                return;
+            }
+
+            Tribe tribe = session.Player.Tribesman.Tribe;
+            using (new MultiObjectLock(tribe))
+            {
+                if(tribe.Level>=20)
+                {
+                    ReplyError(session, packet, Error.TribeMaxLevel);
+                    return;
+                }
+                Resource cost = Formula.GetTribeUpgradeCost(tribe.Level);
+                if (!tribe.Resource.HasEnough(cost))
+                {
+                    ReplyError(session, packet, Error.ResourceNotEnough);
+                    return;
+                }
+
+                tribe.Upgrade();
+            }
+
+            ReplySuccess(session, packet);
+        }
+
+        public void CmdTribeIncomingList(Session session, Packet packet)
+        {
+            /* Tribe t;
+             List<NotificationManager.Notification> notifications;
+             //t.Where(x => x.Player.GetCityList().Where(y => y.Worker.Notifications.Where(z => z.Action is AttackChainAction && z.Subscriptions.Any(city => city == y))));
+             foreach (var city in t.SelectMany(tribesman => tribesman.Player.GetCityList()))
+             {
+                 notifications = new List<NotificationManager.Notification>(city.Worker.Notifications.Where(x => x.Action is AttackChainAction && x.Subscriptions.Any(y => y == city)));
+             }*/
+        }
+
+    }
+}
---
+++ Game/Logic/Actions/TroopActions/UnitUpgradeActiveAction.cs
@@ -0,0 +1,165 @@
+#region
+
+using System;
+using System.Collections.Generic;
+using Game.Data;
+using Game.Data.Stats;
+using Game.Logic.Formulas;
+using Game.Setup;
+using Game.Util;
+
+#endregion
+
+namespace Game.Logic.Actions
+{
+    class UnitUpgradeActiveAction : ScheduledActiveAction
+    {
+        private readonly uint cityId;
+        private readonly uint structureId;
+
+        public UnitUpgradeActiveAction(uint cityId, uint structureId, ushort type)
+        {
+            this.cityId = cityId;
+            this.structureId = structureId;
+            UnitType = type;
+        }
+
+        public UnitUpgradeActiveAction(uint id,
+                                 DateTime beginTime,
+                                 DateTime nextTime,
+                                 DateTime endTime,
+                                 int workerType,
+                                 byte workerIndex,
+                                 ushort actionCount,
+                                 Dictionary<string, string> properties) : base(id, beginTime, nextTime, endTime, workerType, workerIndex, actionCount)
+        {
+            UnitType = ushort.Parse(properties["type"]);
+            cityId = uint.Parse(properties["city_id"]);
+            structureId = uint.Parse(properties["structure_id"]);
+        }
+
+        public ushort UnitType { get; private set; }
+
+        public override ConcurrencyType Concurrency
+        {
+            get
+            {
+                return ConcurrencyType.Normal;
+            }
+        }
+
+        public override ActionType Type
+        {
+            get
+            {
+                return ActionType.UnitUpgradeActive;
+            }
+        }
+
+        public override Error Validate(string[] parms)
+        {
+            City city;
+            Structure structure;
+            if (!Global.World.TryGetObjects(cityId, structureId, out city, out structure))
+                return Error.ActionInvalid;
+
+            if (ushort.Parse(parms[0]) != UnitType || byte.Parse(parms[1]) <= city.Template[UnitType].Lvl)
+                return Error.ActionInvalid;
+
+            return Error.Ok;
+        }
+
+        public override Error Execute()
+        {
+            City city;
+            Structure structure;
+            if (!Global.World.TryGetObjects(cityId, structureId, out city, out structure))
+                return Error.ObjectNotFound;
+
+            if (city.Worker.ActiveActions.Exists(action => (action.Type == Type) && (UnitType == ((UnitUpgradeActiveAction)action).UnitType) && (action != this)))
+                return Error.ActionAlreadyInProgress;
+
+            BaseUnitStats unitStats = city.Template[UnitType];
+            Resource cost = Formula.UnitUpgradeCost(city, UnitType, (byte)(unitStats.Lvl + 1));
+
+            if (cost == null)
+                return Error.ObjectNotFound;
+            if (!city.Resource.HasEnough(cost))
+                return Error.ResourceNotEnough;
+
+            city.BeginUpdate();
+            city.Resource.Subtract(cost);
+            city.EndUpdate();
+
+            var upgradeTime = Formula.BuildTime(UnitFactory.GetUpgradeTime(UnitType, (byte)(unitStats.Lvl + 1)), city, structure.Technologies);
+            endTime = DateTime.UtcNow.AddSeconds(CalculateTime(upgradeTime));
+            BeginTime = DateTime.UtcNow;
+
+            return Error.Ok;
+        }
+
+        private void InterruptCatchAll(bool wasKilled)
+        {
+            City city;
+            using (new MultiObjectLock(cityId, out city))
+            {
+                if (!IsValid())
+                    return;
+
+                if (!wasKilled)
+                {
+                    city.BeginUpdate();
+                    city.Resource.Add(Formula.GetActionCancelResource(BeginTime, UnitFactory.GetUpgradeCost(UnitType, city.Template[UnitType].Lvl + 1)));
+                    city.EndUpdate();
+                }
+
+                StateChange(ActionState.Failed);
+            }
+        }
+
+        public override void UserCancelled()
+        {
+            InterruptCatchAll(false);
+        }
+
+        public override void WorkerRemoved(bool wasKilled)
+        {
+            InterruptCatchAll(wasKilled);
+        }
+
+        public override void Callback(object custom)
+        {
+            City city;
+            Structure structure;
+            using (new MultiObjectLock(cityId, out city))
+            {
+                if (!IsValid())
+                    return;
+
+                if (!city.TryGetStructure(structureId, out structure))
+                {
+                    StateChange(ActionState.Failed);
+                    return;
+                }
+
+                structure.City.Template[UnitType] = UnitFactory.GetUnitStats(UnitType, (byte)(structure.City.Template[UnitType].Lvl + 1));
+
+                StateChange(ActionState.Completed);
+            }
+        }
+
+        #region IPersistable
+
+        public override string Properties
+        {
+            get
+            {
+                return
+                        XmlSerializer.Serialize(new[]
+                                                {new XmlKvPair("type", UnitType), new XmlKvPair("city_id", cityId), new XmlKvPair("structure_id", structureId)});
+            }
+        }
+
+        #endregion
+    }
+}

cf5d02af9b03a37101ed484ea607718c2dd5d967
Giuliano Barberi <gbarberi@aotaonline.com>
2011-10-02 12:47:46 -0400

    Fixing group size Max() when empty bug

--- Game/Battle/CombatUnitFactory.cs
+++ Game/Battle/CombatUnitFactory.cs
@@ -36,10 +36,12 @@ namespace Game.Battle
         {
             BaseUnitStats template = stub.City.Template[type];
             BattleStats stats = stub.Template[type];
-            var groupSize = (from effect in stub.City.Technologies.GetEffects(EffectCode.UnitStatMod, EffectInheritance.All)
-                             where effect.Id == EffectCode.UnitStatMod
-                             where BattleFormulas.UnitStatModCheck(stats.Base, TroopBattleGroup.Defense, (string)effect.Value[3])
-                             select (int)effect.Value[2]).Max() + stats.Base.GroupSize;
+            var mods = (from effect in stub.City.Technologies.GetEffects(EffectCode.UnitStatMod, EffectInheritance.All)
+                        where effect.Id == EffectCode.UnitStatMod
+                        where BattleFormulas.UnitStatModCheck(stats.Base, TroopBattleGroup.Defense, (string)effect.Value[3])
+                        select (int)effect.Value[2]).DefaultIfEmpty().Max();
+
+            var groupSize = mods + stats.Base.GroupSize;
 
             var units = new DefenseCombatUnit[(count - 1) / groupSize + 1];
             DefenseCombatUnit newUnit;

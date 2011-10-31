3f6a216f3fc2815ad94b0f6007411f1b441e1b9a
Giuliano Barberi <gbarberi@aotaonline.com>
2011-10-31 19:37:06 -0400

    Changed attack point requirement message on website.

---
+++ Game/Comm/CmdLineCommands.cs
@@ -0,0 +1,28 @@
+﻿namespace Game.Comm
+{
+    public enum CmdLineCommand
+    {
+        SendResources = 100,
+        Ban = 110,
+        Unban = 111,
+        Delete = 112,
+
+        PlayerClearDescription = 120,
+        DeleteInactives = 121,
+        BroadCast = 122,
+
+        TribeInfo = 201,
+        TribeCreate = 202,
+        TribeDelete = 203,
+        TribeUpdate = 204,
+        TribeUpgrade = 205,
+        TribesmanAdd = 211,
+        TribesmanRemove = 212,
+        TribesmanUpdate = 213,
+        TribeIncomingList = 214,
+
+        AssignmentList = 221,
+        AssignmentCreate = 222,
+        AssignmentJoin = 223,
+    }
+}
---
+++ Game/Logic/Procedures/OnActionCallback.cs
@@ -0,0 +1,27 @@
+﻿#region
+
+using Game.Data;
+using Game.Logic.Formulas;
+using Game.Setup;
+using Ninject;
+
+#endregion
+
+namespace Game.Logic.Procedures
+{
+    public partial class Procedure
+    {
+        public static void RecalculateCityResourceRates(City city)
+        {
+            city.Resource.Crop.Rate = Formula.GetCropRate(city);
+            city.Resource.Iron.Rate = Formula.GetIronRate(city);
+            city.Resource.Wood.Rate = Formula.GetWoodRate(city);
+        }
+
+        public static void OnStructureUpgradeDowngrade(Structure structure)
+        {
+            SetResourceCap(structure.City);
+            RecalculateCityResourceRates(structure.City);
+        }
+    }
+}

adb8bd9579943c8e51242355e638746cb98a64e6
gbarberi <gbarberi@localhost>
2011-11-03 21:26:32 -0400

    Removing unused validation from players

--- app/models/player.php
+++ app/models/player.php
@@ -3,25 +3,6 @@
 class Player extends AppModel {
 
     var $name = 'Player';
-    var $validate = array(
-        'name' => array(
-            array('rule' => 'notEmpty', 'required' => true, 'on' => 'create'),
-            array('rule' => array('between', 5, 32), 'message' => 'Name must be between 5 and 32 characters in length'),
-            array('rule' => 'alphaNumeric', 'message' => 'Player name must only contain letters and numbers'),
-            array('rule' => 'isUnique', 'message' => 'A player with this name already exists'),
-            array('rule' => '/^((?!system).)*$/i', 'message' => 'Illegal player name. Player names cannot contain the word "System"'),
-        ),
-        'email_address' => array(
-            array('rule' => 'notEmpty', 'required' => true, 'on' => 'create'),
-            array('rule' => 'isUnique', 'message' => 'This email address has already been registered'),
-            array('rule' => array('email', false), 'message' => 'This is not a valid email address'),
-        ),
-        'password_once' => array(
-            array('rule' => 'notEmpty', 'required' => true, 'on' => 'create'),
-            array('rule' => array('match', 'password_twice'), 'message' => 'Passwords must match'),
-            array('rule' => array('between', 5, 32), 'message' => 'Must be between 5 and 32 characters long'),
-        )
-    );
     var $hasOne = array(
         'City',
         'Tribesman'
@@ -47,10 +28,10 @@ class Player extends AppModel {
 
     public function getTribeId($playerId) {
         $tribesman = $this->Tribesman->findByPlayerId($playerId);
-        if (empty($tribesman)) 
+        if (empty($tribesman))
             return null;
-        
+
         return $tribesman['Tribesman']['tribe_id'];
-    }   
+    }
 
 }

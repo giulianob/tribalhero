305f7aa0c108f6ea90f1164a9f7481c302534e31
anthony <anthony@localhost>
2011-11-06 17:45:52 -0500

    Initial commit

---
+++ Game/Logic/Procedures/ResourceCap.cs
@@ -0,0 +1,35 @@
+﻿#region
+
+using Game.Data;
+using Game.Logic.Formulas;
+using Game.Setup;
+using System.Linq;
+using Ninject;
+
+#endregion
+
+namespace Game.Logic.Procedures
+{
+    partial class Procedure
+    {
+        /// <summary>
+        ///   Sets the resource caps for the given city
+        /// </summary>
+        /// <param name = "city"></param>
+        public static void SetResourceCap(City city)
+        {
+            var bonus = city.Technologies.GetEffects(EffectCode.AtticStorageMod, EffectInheritance.All).Sum(x => (int)x.Value[0]);
+            var resourceBonus = Formula.HiddenResource(city) * (double)bonus / 100;
+            if (Config.resource_cap)
+            {
+                city.Resource.SetLimits(Formula.ResourceCropCap(city.Lvl) + resourceBonus.Crop,
+                                        0,
+                                        Formula.ResourceIronCap(city.Lvl) + resourceBonus.Iron,
+                                        Formula.ResourceWoodCap(city.Lvl) + resourceBonus.Wood,
+                                        0);
+            }
+            else
+                city.Resource.SetLimits(int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue);
+        }
+    }
+}

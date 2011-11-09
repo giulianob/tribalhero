592ffeec95268b50280d7febe438772a404a3fae
anthony <anthony@localhost>
2011-11-09 01:59:34 -0500

    Change IdleChecker to Player Remover, refactored certain part of it.

---
+++ Game/Util/Locking/Concurrency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Util.Locking
+{
+    public static class Concurrency
+    {
+        private static ILocker locker = new DefaultLocker(() => new TransactionalMultiObjectLock(new DefaultMultiObjectLock()));
+
+        public static ILocker Current
+        {
+            get
+            {
+                return locker;
+            }
+            set
+            {
+                locker = value;
+            }
+        }
+    }
+}

3c1fde195b058e37674e96c2fc66f9a8af66e501
gbarberi <gbarberi@localhost>
2011-11-12 09:59:15 -0500

    Changed delete command to deleteplayer Changed deleteinactives to deletenewbies

--- Game/Module/Remover/NewbieIdleSelector.cs
+++ Game/Module/Remover/NewbieIdleSelector.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Game.Data;
+﻿using System.Collections.Generic;
 using Game.Setup;
-using Game.Util.Locking;
 using Ninject;
 using Persistance;
 

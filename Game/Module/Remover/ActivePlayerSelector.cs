0a2fa596a523d1bdf8bc0408887de261e822ca9a
anthony <anthony@localhost>
2011-11-17 20:07:06 -0500

    Change Double time to take effect when it's 500 squares away

---
+++ Game/Module/Remover/ActivePlayerSelector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Game.Data;
+using Game.Setup;
+using Ninject;
+using Persistance;
+
+namespace Game.Module.Remover {
+    internal class ActivePlayerSelector: IPlayerSelector {
+        int maxIdleDays;
+
+        public ActivePlayerSelector(int maxIdleDays)
+        {
+            this.maxIdleDays = maxIdleDays;
+        }
+
+        public IEnumerable<uint> GetPlayerIds()
+        {
+            using (var reader =
+                            Ioc.Kernel.Get<IDbManager>().ReaderQuery(
+                                                                    string.Format(
+                                                                       "SELECT players.id player_id FROM `{0}` WHERE TIMEDIFF(UTC_TIMESTAMP(), `last_login`) < '{1}:00:00.000000'",
+                                                                       Player.DB_TABLE,
+                                                                       maxIdleDays * 24
+                                        ), new DbColumn[] { })) {
+                while (reader.Read())
+                    yield return (uint)reader["player_id"];
+            }
+        }
+    }
+}

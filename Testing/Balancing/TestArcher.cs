42fedd7405ff0ce11ed2bcd660f95862186ccbf0
anthony <anthony@localhost>
2011-11-18 16:56:44 -0500

    Fixed canceling action always return full cost even after 60 seconds.

---
+++ Testing/Balancing/TestArcher.cs
@@ -0,0 +1,65 @@
+﻿#region
+
+using System;
+using ConsoleSimulator;
+using Game.Data;
+using Game.Logic.Procedures;
+using Game.Setup;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Game.Battle;
+using Ninject;
+using Persistance;
+using Testing.Balancing;
+
+#endregion
+
+namespace Testing.Troop
+{
+    /// <summary>
+    ///   Summary description for TroopProcedureTest
+    /// </summary>
+    [TestClass]
+    public class TestArcher : TestBase
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void Test10AcherDef10Swordsman()
+        {
+            TestUtility.TestUnitPerUpkeep(UnitType.Archer, UnitType.Fighter, 10, .4);
+        }
+
+        [TestMethod]
+        public void Test10SwordsmanDef10Swordsman() {
+            TestUtility.TestUnitPerUpkeep(UnitType.Swordsman, UnitType.Swordsman, 10, -0.2);
+        }
+        [TestMethod]
+        public void Test10SwordsmanDef10Archer() {
+            TestUtility.TestUnitPerUpkeep(UnitType.Swordsman, UnitType.Archer, 10, .3);
+        }
+        [TestMethod]
+        public void Test10SwordsmanDef10Pikeman() {
+            TestUtility.TestUnitPerUpkeep(UnitType.Swordsman, UnitType.Pikeman, 10, .5);
+        }
+        [TestMethod]
+        public void Test10SwordsmanDef10Gladiator() {
+            TestUtility.TestUnitPerUpkeep(UnitType.Swordsman, UnitType.Gladiator, 10, -0.3);
+        }
+        [TestMethod]
+        public void Test10SwordsmanDef10Cavalry() {
+            TestUtility.TestUnitPerUpkeep(UnitType.Swordsman, UnitType.Pikeman, 10, -0.7);
+        }
+        [TestMethod]
+        public void Test10SwordsmanDef10Knight() {
+            TestUtility.TestUnitPerUpkeep(UnitType.Swordsman, UnitType.Pikeman, 10, -0.4);
+        }
+    }
+}
---
+++ Testing/Lock/CallbackLockTest.cs
@@ -0,0 +1,182 @@
+﻿#region
+
+using Game.Util;
+using Game.Util.Locking;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+
+namespace Testing.Lock
+{
+    /// <summary>
+    ///   Summary description for TroopProcedureTest
+    /// </summary>
+    [TestClass]
+    public class CallbackLockTest : TestBase
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void TestEmptyListFromCallback()
+        {
+            var obj = new DummyLockable(1);
+            CallbackLock.CallbackLockHandler lockFunc = custom => new ILockable[] {};
+            var lck = new CallbackLock().Lock(lockFunc, null, obj);
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj));
+            lck.Dispose();
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj));
+        }
+
+        [TestMethod]
+        public void TestSingleItemFromCallback()
+        {
+            var obj = new DummyLockable(1);
+            var obj2 = new DummyLockable(2);
+            CallbackLock.CallbackLockHandler lockFunc = custom => new ILockable[] {obj2};
+            var lck = new CallbackLock().Lock(lockFunc, null, obj);
+
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj));
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj2));
+
+            lck.Dispose();
+
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj));
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj2));
+        }
+
+        [TestMethod]
+        public void TestMultiItemFromCallback()
+        {
+            var obj = new DummyLockable(1);
+            var obj2 = new DummyLockable(2);
+            var obj3 = new DummyLockable(3);
+            CallbackLock.CallbackLockHandler lockFunc = custom => new ILockable[] {obj3, obj2};
+            var lck = new CallbackLock().Lock(lockFunc, null, obj);
+
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj));
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj2));
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj3));
+
+            lck.Dispose();
+
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj));
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj2));
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj3));
+        }
+
+        [TestMethod]
+        public void TestMultiBaseItems()
+        {
+            var obj = new DummyLockable(1);
+            var obj2 = new DummyLockable(2);
+            CallbackLock.CallbackLockHandler lockFunc = custom => new ILockable[] {};
+            var lck = new CallbackLock().Lock(lockFunc, null, obj, obj2);
+
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj));
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj2));
+
+            lck.Dispose();
+
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj));
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj2));
+        }
+
+        [TestMethod]
+        public void TestChangingCallbackAddLocks()
+        {
+            var obj = new DummyLockable(1);
+            var obj2 = new DummyLockable(2);
+            var obj3 = new DummyLockable(3);
+
+            int callCount = 0;
+            CallbackLock.CallbackLockHandler lockFunc = delegate
+                {
+                    if (callCount == 0)
+                    {
+                        callCount++;
+                        return new ILockable[] {obj2};
+                    }
+
+                    callCount++;
+                    return new ILockable[] {obj3, obj2};
+                };
+            var lck = new CallbackLock().Lock(lockFunc, null, obj);
+
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj));
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj2));
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj3));
+
+            lck.Dispose();
+
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj));
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj2));
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj3));
+        }
+
+        [TestMethod]
+        public void TestChangingCallbackRemoveLocks()
+        {
+            var obj = new DummyLockable(1);
+            var obj2 = new DummyLockable(2);
+            var obj3 = new DummyLockable(3);
+
+            int callCount = 0;
+            CallbackLock.CallbackLockHandler lockFunc = delegate
+                {
+                    if (callCount == 0)
+                    {
+                        callCount++;
+                        return new ILockable[] {obj2, obj3};
+                    }
+
+                    callCount++;
+                    return new ILockable[] {obj2};
+                };
+            var lck = new CallbackLock().Lock(lockFunc, null, obj);
+
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj));
+            Assert.IsTrue(DefaultMultiObjectLock.IsLocked(obj2));
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj3));
+
+            lck.Dispose();
+
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj));
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj2));
+            Assert.IsFalse(DefaultMultiObjectLock.IsLocked(obj3));
+        }
+
+        #region Nested type: DummyLockable
+
+        private class DummyLockable : ILockable
+        {
+            public DummyLockable(int id)
+            {
+                Hash = id;
+            }
+
+            #region ILockable Members
+
+            public int Hash { get; private set; }
+
+            public object Lock
+            {
+                get
+                {
+                    return this;
+                }
+            }
+
+            #endregion
+        }
+
+        #endregion
+    }
+}

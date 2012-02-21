122977ccd08839e2ecc857f2c3e3324abb9f8c7b
Giuliano Barberi <gbarberi@aotaonline.com>
2012-02-20 20:20:43 -0500

    Added temporary structures to not be counted in city value. Removed all Ioc references from Formula classes so now they are all properly injected and added their dependencies. Fixed broken tests. Added balancing tests to ignore.

---
+++ Game/Comm/CmdLine Commands/AssignmentCommandLineModule.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Linq;
+using Game.Data;
+using Game.Data.Tribe;
+using Game.Data.Troop;
+using Game.Database;
+using Game.Logic.Actions;
+using Game.Logic.Procedures;
+using Game.Map;
+using Game.Setup;
+using Game.Util;
+using Game.Util.Locking;
+using NDesk.Options;
+
+namespace Game.Comm
+{
+    class AssignmentCommandLineModule : CommandLineModule
+    {
+        public override void RegisterCommands(CommandLineProcessor processor)
+        {
+            processor.RegisterCommand("assignmentlist", AssignmentList, true);
+            processor.RegisterCommand("assignmentcreate", AssignmentCreate, true);
+            processor.RegisterCommand("assignmentjoin", AssignmentJoin, true);
+        }
+
+        private string AssignmentList(Session session, string[] parms)
+        {
+            bool help = false;
+            string playerName = string.Empty;
+            string tribeName = string.Empty;
+
+            try
+            {
+                var p = new OptionSet
+                        {
+                                {"?|help|h", v => help = true},
+                                {"player=", v => playerName = v.TrimMatchingQuotes()},
+                                {"tribe=", v => tribeName = v.TrimMatchingQuotes()},
+                        };
+                p.Parse(parms);
+            }
+            catch (Exception)
+            {
+                help = true;
+            }
+
+            if (help || string.IsNullOrEmpty(playerName) && string.IsNullOrEmpty(tribeName))
+                return "AssignmentList --player=player_name|--tribe=tribe_name";
+
+            uint playerId;
+            if (!string.IsNullOrEmpty(playerName))
+            {
+                if (!World.Current.FindPlayerId(playerName, out playerId))
+                    return "Player not found";
+            }
+            else
+            {
+                if (!World.Current.FindTribeId(tribeName, out playerId))
+                    return "Tribe not found";
+            }
+
+            IPlayer player;
+            ITribe tribe;
+            string result = string.Format("Now[{0}] Assignments:\n", DateTime.UtcNow);
+            using (Concurrency.Current.Lock(playerId, out player, out tribe))
+            {
+                if (player == null)
+                    return "Player not found";
+                if (tribe == null)
+                    return "Player does not own a tribe";
+
+                result = tribe.Assignments.Aggregate(result, (current, assignment) => current + assignment.ToNiceString());
+            }
+
+            return result;
+        }
+
+        private string AssignmentCreate(Session session, string[] parms)
+        {
+            bool help = false;
+            string cityName = string.Empty;
+            uint x = 0;
+            uint y = 0;
+            TimeSpan time = TimeSpan.MinValue;
+            AttackMode mode = AttackMode.Normal;
+            try
+            {
+                var p = new OptionSet
+                        {
+                                {"?|help|h", v => help = true},
+                                {"city=", v => cityName = v.TrimMatchingQuotes()},
+                                {"x=", v => x = uint.Parse(v)},
+                                {"y=", v => y = uint.Parse(v)},
+                                {"timespan=", v => time = TimeSpan.Parse(v.TrimMatchingQuotes())},
+                                {"mode=", v => mode = (AttackMode)Enum.Parse(typeof(AttackMode), v)},
+                        };
+                p.Parse(parms);
+            }
+            catch (Exception)
+            {
+                help = true;
+            }
+
+            if (help || string.IsNullOrEmpty(cityName) || x == 0 || y == 0 || time == TimeSpan.MinValue)
+                return "AssignmentCreate --city=city_name --x=x --y=y --timespan=0:0:0 [--mode=attack_mode]";
+
+            uint cityId;
+            if (!World.Current.FindCityId(cityName, out cityId))
+                return "City not found";
+
+            ICity city;
+            if (!World.Current.TryGetObjects(cityId, out city))
+            {
+                return "City not found!";
+            }
+
+            if (city.Owner.Tribesman == null)
+            {
+                return "Not in tribe";
+            }
+
+            ITribe tribe = city.Owner.Tribesman.Tribe;
+            IStructure targetStructure = World.Current.GetObjects(x, y).OfType<IStructure>().First();           
+            if (targetStructure == null)
+            {
+                return "Could not find a structure for the given coordinates";
+            }
+
+            using (Concurrency.Current.Lock(city, tribe, targetStructure.City))
+            {
+                if (city.DefaultTroop.Upkeep == 0)
+                {
+                    return "No troops in the city!";
+                }
+
+                TroopStub stub = new TroopStub { city.DefaultTroop };
+                Procedure.Current.TroopStubCreate(city, stub, TroopState.WaitingInAssignment);
+                DbPersistance.Current.Save(stub);
+
+                targetStructure = World.Current.GetObjects(x, y).OfType<IStructure>().First();
+
+                if (targetStructure == null)
+                {
+                    return "Could not find a structure for the given coordinates";
+                }
+
+                int id;
+                Error error = tribe.CreateAssignment(stub, x, y, targetStructure.City, DateTime.UtcNow.Add(time), mode, "", out id);
+                if (error != Error.Ok)
+                {
+                    city.Troops.Remove(stub.TroopId);
+                    return Enum.GetName(typeof(Error), error);
+                }
+
+                return string.Format("OK ID[{0}]", id);
+            }
+        }
+
+        private string AssignmentJoin(Session session, string[] parms)
+        {
+            bool help = false;
+            string cityName = string.Empty;
+            int id = int.MaxValue;
+            try
+            {
+                var p = new OptionSet { { "?|help|h", v => help = true }, { "city=", v => cityName = v.TrimMatchingQuotes() }, { "id=", v => id = int.Parse(v) }, };
+                p.Parse(parms);
+            }
+            catch (Exception)
+            {
+                help = true;
+            }
+
+            if (help || string.IsNullOrEmpty(cityName) || id == int.MaxValue)
+                return "AssignementCreate --city=city_name --id=id";
+
+            uint cityId;
+            if (!World.Current.FindCityId(cityName, out cityId))
+                return "City not found";
+
+            ICity city;
+            TroopStub stub = new TroopStub();
+            if (!World.Current.TryGetObjects(cityId, out city))
+            {
+                return "City not found!";
+            }
+
+            if (city.Owner.Tribesman == null)
+            {
+                return "Not in tribe";
+            }
+
+            ITribe tribe = city.Owner.Tribesman.Tribe;
+            using (Concurrency.Current.Lock(city, tribe))
+            {
+                if (city.DefaultTroop.Upkeep == 0)
+                {
+                    return "No troops in the city!";
+                }
+                stub.Add(city.DefaultTroop);
+                Procedure.Current.TroopStubCreate(city, stub, TroopState.WaitingInAssignment);
+                DbPersistance.Current.Save(stub);
+
+                Error error = tribe.JoinAssignment(id, stub);
+                if (error != Error.Ok)
+                {
+                    Procedure.Current.TroopStubDelete(city, stub);
+                    return Enum.GetName(typeof(Error), error);
+                }
+
+                return string.Format("OK");                
+            }
+        }
+    }
+}
---
+++ Game/Logic/Actions/StructureActions/StructureDowngradeActiveAction.cs
@@ -0,0 +1,198 @@
+#region
+
+using System;
+using System.Collections.Generic;
+using Game.Data;
+using Game.Logic.Formulas;
+using Game.Logic.Procedures;
+using Game.Map;
+using Game.Setup;
+using Game.Util;
+using Game.Util.Locking;
+using Ninject;
+
+#endregion
+
+namespace Game.Logic.Actions
+{
+    class StructureDowngradeActiveAction : ScheduledActiveAction
+    {
+        private readonly uint cityId;
+        private readonly uint structureId;
+
+        public StructureDowngradeActiveAction(uint cityId, uint structureId)
+        {
+            this.cityId = cityId;
+            this.structureId = structureId;
+        }
+
+        public StructureDowngradeActiveAction(uint id,
+                                            DateTime beginTime,
+                                            DateTime nextTime,
+                                            DateTime endTime,
+                                            int workerType,
+                                            byte workerIndex,
+                                            ushort actionCount,
+                                            Dictionary<string, string> properties)
+                : base(id, beginTime, nextTime, endTime, workerType, workerIndex, actionCount)
+        {
+            cityId = uint.Parse(properties["city_id"]);
+            structureId = uint.Parse(properties["structure_id"]);
+        }
+
+        public override ConcurrencyType ActionConcurrency
+        {
+            get
+            {
+                return ConcurrencyType.Normal;
+            }
+        }
+
+        public override ActionType Type
+        {
+            get
+            {
+                return ActionType.StructureDowngradeActive;
+            }
+        }
+
+        public override Error Execute()
+        {
+            ICity city;
+            IStructure structure;
+
+            if (!World.Current.TryGetObjects(cityId, structureId, out city, out structure))
+                return Error.ObjectNotFound;
+
+            if (Ioc.Kernel.Get<ObjectTypeFactory>().IsStructureType("MainBuilding", structure))
+                return Error.StructureUndowngradable;
+
+            if (Ioc.Kernel.Get<ObjectTypeFactory>().IsStructureType("Unattackable", structure))
+                return Error.StructureUndowngradable;
+
+            if (Ioc.Kernel.Get<ObjectTypeFactory>().IsStructureType("Undestroyable", structure) && structure.Lvl <= 1)
+                return Error.StructureUndestroyable;
+
+            endTime =
+                    DateTime.UtcNow.AddSeconds(
+                                               CalculateTime(Formula.Current.BuildTime(Ioc.Kernel.Get<StructureFactory>().GetTime(structure.Type, (byte)(structure.Lvl + 1)),
+                                                                               city,
+                                                                               structure.Technologies)));
+            BeginTime = DateTime.UtcNow;
+
+            if (WorkerObject.WorkerId != structureId)
+                city.Worker.References.Add(structure, this);
+
+            return Error.Ok;
+        }
+
+        public override void Callback(object custom)
+        {
+            ICity city;
+            IStructure structure;
+
+            // Block structure
+            using (Concurrency.Current.Lock(cityId, structureId, out city, out structure))
+            {
+                if (!IsValid())
+                    return;
+
+                if (structure == null)
+                {
+                    StateChange(ActionState.Completed);
+                    return;
+                }
+
+                if (WorkerObject.WorkerId != structureId)
+                {
+                    city.Worker.References.Remove(structure, this);
+                }
+
+                if (structure.IsBlocked)
+                {
+                    StateChange(ActionState.Failed);
+                    return;
+                }
+
+                if (Ioc.Kernel.Get<ObjectTypeFactory>().IsStructureType("Undestroyable", structure))
+                {
+                    StateChange(ActionState.Failed);
+                    return;
+                }
+
+                if (structure.State.Type == ObjectState.Battle)
+                {
+                    StateChange(ActionState.Failed);
+                    return;
+                }
+
+                structure.BeginUpdate();
+                structure.IsBlocked = true;
+                structure.EndUpdate();
+            }
+
+            structure.City.Worker.Remove(structure, new GameAction[] {this});
+
+            using (Concurrency.Current.Lock(cityId, structureId, out city, out structure))
+            {
+                city.BeginUpdate();
+                structure.BeginUpdate();
+
+                // Unblock structure since we're done with it in this action and ObjectRemoveAction will take it from here
+                structure.IsBlocked = false;
+
+                // Destroy structure
+                World.Current.Remove(structure);
+                city.ScheduleRemove(structure, false);
+
+                structure.EndUpdate();
+                city.EndUpdate();
+
+                StateChange(ActionState.Completed);
+            }
+        }
+
+        public override Error Validate(string[] parms)
+        {
+            return Error.Ok;
+        }
+
+        private void InterruptCatchAll(bool wasKilled)
+        {
+            ICity city;
+            using (Concurrency.Current.Lock(cityId, out city))
+            {
+                if (!IsValid())
+                    return;
+
+                IStructure structure;
+                if (WorkerObject.WorkerId != structureId && city.TryGetStructure(structureId, out structure))
+                    city.Worker.References.Remove(structure, this);
+
+                StateChange(ActionState.Failed);
+            }
+        }
+
+        public override void UserCancelled()
+        {
+            InterruptCatchAll(false);
+        }
+
+        public override void WorkerRemoved(bool wasKilled)
+        {
+            InterruptCatchAll(wasKilled);
+        }
+
+        #region IPersistable
+
+        public override string Properties
+        {
+            get
+            {
+                return XmlSerializer.Serialize(new[] {new XmlKvPair("city_id", cityId), new XmlKvPair("structure_id", structureId)});
+            }
+        }
+
+        #endregion
+    }
+}

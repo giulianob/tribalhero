adb3486bbe29fa73256ccff8dd78250b7997a5ff
anthony <anthony@localhost>
2012-04-08 17:07:14 -0400

    Moved contribute functionality from TribePanel to Trading Post.

---
+++ Game/Util/Locking/TransactionalMultiObjectLock.cs
@@ -0,0 +1,39 @@
+﻿using Game.Database;
+using Game.Setup;
+using Ninject;
+using Persistance;
+
+namespace Game.Util.Locking
+{
+    class TransactionalMultiObjectLock : IMultiObjectLock
+    {
+        private readonly IMultiObjectLock lck;
+
+        public TransactionalMultiObjectLock(IMultiObjectLock lck)
+        {
+            this.lck = lck;
+        }
+
+        public void Lock(params ILockable[] list)
+        {            
+            lck.Lock(list);
+            DbPersistance.Current.GetThreadTransaction();
+        }
+
+        public void UnlockAll()
+        {
+            var transaction = DbPersistance.Current.GetThreadTransaction(true);
+            if (transaction != null)
+            {
+                transaction.Dispose();
+            }
+
+            lck.UnlockAll();
+        }
+
+        public void Dispose()
+        {
+            UnlockAll();
+        }
+    }
+}

b66cff955c53d5239366578a01b1e0836767c6c6
anthony <anthony@localhost>
2012-04-12 19:53:02 -0400

    Added structure destroy action to level Town center.

---
+++ Game/Battle/BattleFormulas.cs
@@ -0,0 +1,287 @@
+#region
+
+using System;
+using System.Linq;
+using Game.Data;
+using Game.Data.Stats;
+using Game.Data.Troop;
+using Game.Setup;
+using System.Collections.Generic;
+using Ninject;
+
+#endregion
+
+namespace Game.Battle
+{
+    public class BattleFormulas
+    {
+        private readonly UnitModFactory unitModFactory;
+
+        private readonly UnitFactory unitFactory;
+
+        public static BattleFormulas Current { get; set; }
+
+        public BattleFormulas()
+        {
+        }
+
+        public BattleFormulas(UnitModFactory unitModFactory, UnitFactory unitFactory)
+        {
+            this.unitModFactory = unitModFactory;
+            this.unitFactory = unitFactory;
+        }
+
+        public virtual int MissChance(int attackersUpkeep, int defendersUpkeep)
+        {
+            double delta = Math.Max(0, (double)attackersUpkeep / defendersUpkeep);
+            double effectiveness = attackersUpkeep > 200 ? 1 : (double)attackersUpkeep / 200;
+         
+            if (delta < 1) return (int)(0 * effectiveness);
+            if (delta < 1.25) return (int)(10 * effectiveness);
+            if (delta < 1.5) return (int)(17 * effectiveness);
+            if (delta < 2) return (int)(22 * effectiveness);
+            if (delta < 3.5) return (int)(30 * effectiveness);
+            if (delta < 5) return (int)(40 * effectiveness);
+            if (delta < 7) return (int)(48 * effectiveness);
+            if (delta < 10) return (int)(55 * effectiveness);
+            return (int)(60 * effectiveness);
+        }
+
+        public virtual int GetUnitsPerStructure(IStructure structure)
+        {
+            var units = new[] { 20, 20, 23, 28, 34, 39, 45, 52, 59, 67, 76, 85, 95, 106, 117, 130 };
+            return units[structure.Lvl];
+        }
+
+        public virtual decimal GetDamage(CombatObject attacker, CombatObject target, bool useDefAsAtk)
+        {
+            decimal atk = attacker.Stats.Atk;
+            decimal rawDmg = (atk * attacker.Count);
+            decimal modifier = (decimal)GetDmgModifier(attacker, target);
+            rawDmg = modifier*rawDmg;
+
+            return rawDmg > ushort.MaxValue ? ushort.MaxValue : rawDmg;
+        }
+
+        public virtual double GetDmgModifier(CombatObject attacker, CombatObject target) {
+            switch(attacker.Stats.Base.Weapon)
+            {
+                case WeaponType.Tower:
+                    switch (target.Stats.Base.Armor)
+                    {
+                        case ArmorType.Building1:
+                            return unitModFactory.GetModifier(1, 1);
+                        case ArmorType.Building2:
+                            return unitModFactory.GetModifier(1, 2);
+                        case ArmorType.Building3:
+                            return unitModFactory.GetModifier(1, 3);
+                        default:
+                            return unitModFactory.GetModifier(1, target.Type);
+                    }
+                case WeaponType.Cannon:
+                    switch (target.Stats.Base.Armor)
+                    {
+                        case ArmorType.Building1:
+                            return unitModFactory.GetModifier(2, 1);
+                        case ArmorType.Building2:
+                            return unitModFactory.GetModifier(2, 2);
+                        case ArmorType.Building3:
+                            return unitModFactory.GetModifier(2, 3);
+                        default:
+                            return unitModFactory.GetModifier(2, target.Type);
+                    }
+                case WeaponType.Barricade:
+                    switch (target.Stats.Base.Armor)
+                    {
+                        case ArmorType.Building1:
+                            return unitModFactory.GetModifier(3, 1);
+                        case ArmorType.Building2:
+                            return unitModFactory.GetModifier(3, 2);
+                        case ArmorType.Building3:
+                            return unitModFactory.GetModifier(3, 3);
+                        default:
+                            return unitModFactory.GetModifier(3, target.Type);
+                    }
+                default:
+                    switch (target.Stats.Base.Armor)
+                    {
+                        case ArmorType.Building1:
+                            return unitModFactory.GetModifier(attacker.Type, 1);
+                        case ArmorType.Building2:
+                            return unitModFactory.GetModifier(attacker.Type, 2);
+                        case ArmorType.Building3:
+                            return unitModFactory.GetModifier(attacker.Type, 3);
+                        default:
+                            return unitModFactory.GetModifier(attacker.Type, target.Type);
+                    }
+            }
+        }
+
+        private int GetLootPerRound(ICity city) {
+            double roundsRequired = Math.Max(5, Config.battle_loot_till_full - city.Technologies.GetEffects(EffectCode.LootLoadMod).DefaultIfEmpty().Sum(x => x == null ? 0 : (int)x.Value[0]));
+            return (int)Math.Ceiling(100 / roundsRequired);
+        }
+
+        public virtual Resource GetRewardResource(CombatObject attacker, CombatObject defender)
+        {
+            int totalCarry = attacker.Stats.Carry*attacker.Count;  // calculate total carry, if 10 units with 10 carry, which should be 100
+            int count = Math.Max(1, totalCarry* GetLootPerRound(attacker.City) / 100); // if carry is 100 and % is 5, then count = 5;
+            var spaceLeft = new Resource(totalCarry / Config.resource_crop_ratio,
+                                         totalCarry / Config.resource_gold_ratio,
+                                         totalCarry / Config.resource_iron_ratio,
+                                         totalCarry / Config.resource_wood_ratio,
+                                         totalCarry / Config.resource_labor_ratio); // spaceleft is the maxcarry.
+            spaceLeft.Subtract(attacker.Loot); // maxcarry - current resource is the empty space left.
+            return new Resource(Math.Min(count / Config.resource_crop_ratio, spaceLeft.Crop),  // returning lesser value between the count and the empty space.
+                                Math.Min(count / Config.resource_gold_ratio, spaceLeft.Gold),
+                                Math.Min(count / Config.resource_iron_ratio, spaceLeft.Iron),
+                                Math.Min(count / Config.resource_wood_ratio, spaceLeft.Wood),
+                                0);
+        }
+
+        public virtual short GetStamina(ITroopStub stub, ICity city)
+        {
+            return (short)Config.battle_stamina_initial;
+        }
+
+        public virtual ushort GetStaminaReinforced(ICity city, ushort stamina, uint round)
+        {
+            return stamina;
+        }
+
+        public virtual ushort GetStaminaRoundEnded(ICity city, ushort stamina, uint round)
+        {
+            if (stamina == 0)
+                return 0;
+            return --stamina;
+        }
+
+        public virtual short GetStaminaStructureDestroyed(short stamina, CombatStructure combatStructure)
+        {
+            if (combatStructure.Stats.Base.Armor != ArmorType.Building3)
+                return stamina;
+
+            if (stamina < Config.battle_stamina_destroyed_deduction)
+                return 0;
+
+            return (short)(stamina - Config.battle_stamina_destroyed_deduction);
+        }
+
+        public virtual ushort GetStaminaDefenseCombatObject(ICity city, ushort stamina, uint round)
+        {
+            if (stamina == 0)
+                return 0;
+
+            return --stamina;
+        }
+
+        public virtual bool IsAttackMissed(byte stealth)
+        {
+            return 100 - stealth < Config.Random.Next(0, 100);
+        }
+
+        public virtual bool UnitStatModCheck(BaseBattleStats stats, TroopBattleGroup group, string value) {
+            string[] conditions = value.Split('=', '|');
+            int success = 0;
+            for (int i = 0; i < conditions.Length / 2; ++i) {
+                switch (conditions[i * 2]) {
+                    case "ArmorEqual":
+                        if (stats.Armor == (ArmorType)Enum.Parse(typeof(ArmorType), conditions[i * 2 + 1], true)) ++success;
+                        break;
+                    case "ArmorClassEqual":
+                        if (stats.ArmorClass == (ArmorClass)Enum.Parse(typeof(ArmorClass), conditions[i * 2 + 1], true)) ++success;
+                        break;
+                    case "WeaponEqual":
+                        if (stats.Weapon == (WeaponType)Enum.Parse(typeof(WeaponType), conditions[i * 2 + 1], true)) ++success;
+                        break;
+                    case "WeaponClassEqual":
+                        if (stats.WeaponClass == (WeaponClass)Enum.Parse(typeof(WeaponClass), conditions[i * 2 + 1], true)) ++success;
+                        break;
+                    case "GroupEqual":
+                        switch((TroopBattleGroup)Enum.Parse(typeof(TroopBattleGroup), conditions[i * 2 + 1], true))
+                        {
+                            case TroopBattleGroup.Defense:
+                                if (group == TroopBattleGroup.Local || group == TroopBattleGroup.Defense) ++success;
+                                break;
+                            case TroopBattleGroup.Attack:
+                                if(group==TroopBattleGroup.Attack) ++success;
+                                break;
+                            case TroopBattleGroup.Local:
+                                if (group == TroopBattleGroup.Local) ++success;
+                                break;
+                        }
+                        break;
+                    case "TypeEqual":
+                        if (stats.Type == ushort.Parse(conditions[i * 2 + 1])) ++success;
+                        break;
+                }
+            }
+            return success == conditions.Length / 2;
+        }
+
+        public virtual BattleStats LoadStats(BaseBattleStats stats, ICity city, TroopBattleGroup group)
+        {
+            var calculator = new BattleStatsModCalculator(stats);
+            foreach (var effect in city.Technologies.GetAllEffects()) {
+                if (effect.Id == EffectCode.UnitStatMod) {
+                    if (UnitStatModCheck(stats, group, (string)effect.Value[3])) {
+                        switch ((string)effect.Value[0]) {
+                            case "Atk":
+                                calculator.Atk.AddMod((string)effect.Value[1], (int)effect.Value[2]);
+                                break;
+                            case "Splash":
+                                calculator.Splash.AddMod((string)effect.Value[1], (int)effect.Value[2]);
+                                break;
+                            case "Spd":
+                                calculator.Spd.AddMod((string)effect.Value[1], (int)effect.Value[2]);
+                                break;
+                            case "Stl":
+                                calculator.Stl.AddMod((string)effect.Value[1], (int)effect.Value[2]);
+                                break;
+                            case "Rng":
+                                calculator.Rng.AddMod((string)effect.Value[1], (int)effect.Value[2]);
+                                break;
+                            case "Carry":
+                                calculator.Carry.AddMod((string)effect.Value[1], (int)effect.Value[2]);
+                                break;
+                            case "MaxHp":
+                                calculator.MaxHp.AddMod((string)effect.Value[1], (int)effect.Value[2]);
+                                break;
+                        }
+                    }
+                }
+                else if (effect.Id == EffectCode.ACallToArmMod && group == TroopBattleGroup.Local)
+                {
+                    var bonus = (int)effect.Value[0] * Math.Min(city.Resource.Labor.Value, (int)effect.Value[1]) / ((int)effect.Value[1]);
+                    calculator.Atk.AddMod("CALL_TO_ARM_BONUS", 100 + bonus);
+                }
+            }
+            return calculator.GetStats();
+        }
+
+        public virtual BattleStats LoadStats(IStructure structure)
+        {
+            return LoadStats(structure.Stats.Base.Battle,structure.City,TroopBattleGroup.Local);
+        }
+
+        public virtual BattleStats LoadStats(ushort type, byte lvl, ICity city, TroopBattleGroup group)
+        {
+            return LoadStats(unitFactory.GetUnitStats(type, lvl).Battle,city,group);
+        }
+
+        public virtual Resource GetBonusResources(ITroopObject troop, int originalCount, int remainingCount)
+        {
+            if (originalCount == 0)
+                return new Resource();
+
+            int max = troop.City.Technologies.GetEffects(EffectCode.SunDance, EffectInheritance.Self).DefaultIfEmpty().Sum(x => x != null ? (int)x.Value[0] : 0);
+            float troopsLostPercentage = 1 - remainingCount/(float)originalCount;
+            return new Resource(troop.Stats.Loot)*(troopsLostPercentage)*(1f+(Config.Random.Next(max)/100f));
+        }
+
+        public virtual int GetNumberOfHits(CombatObject currentAttacker)
+        {
+            return currentAttacker.Stats.Splash == 0 ? 1 : currentAttacker.Stats.Splash;
+        }
+    }
+}
---
+++ Game/Comm/CmdLine Commands/CityCommandLineModule.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Linq;
+using Game.Data;
+using Game.Data.Troop;
+using Game.Logic.Actions;
+using Game.Logic.Procedures;
+using Game.Map;
+using Game.Setup;
+using Game.Util;
+using Game.Util.Locking;
+using NDesk.Options;
+
+namespace Game.Comm.CmdLine_Commands
+{
+    class CityCommandLineModule : CommandLineModule
+    {
+        private readonly Procedure procedure;
+
+        public CityCommandLineModule(Procedure procedure)
+        {
+            this.procedure = procedure;
+        }
+
+        public override void RegisterCommands(CommandLineProcessor processor)
+        {
+            processor.RegisterCommand("renamecity", RenameCity, true);
+            processor.RegisterCommand("removestructure", RemoveStructure, true);
+            processor.RegisterCommand("deletestucktroop", DeleteStuckTroop, true);
+        }
+
+        public string DeleteStuckTroop(Session session, String[] parms)
+        {
+            bool help = false;
+            byte stubId = 0;
+            string cityName = string.Empty;
+
+            try
+            {
+                var p = new OptionSet
+                        {
+                                { "?|help|h", v => help = true }, 
+                                { "city=", v => cityName = v.TrimMatchingQuotes() },
+                                { "stubId=", v => stubId = byte.Parse(v.TrimMatchingQuotes()) }
+                        };
+                p.Parse(parms);
+            }
+            catch (Exception)
+            {
+                help = true;
+            }
+
+            if (help || cityName == string.Empty)
+                return "deletestucktroop --city=### --stubId=###";
+
+            uint cityId;
+            if (!World.Current.FindCityId(cityName, out cityId))
+                return "City not found";
+
+            ICity city;
+            using (Concurrency.Current.Lock(cityId, out city))
+            {
+                if (city == null)
+                    return "City not found";
+
+                ITroopStub stub;
+                if (!city.Troops.TryGetStub(stubId, out stub))
+                {
+                    return "Stub not found";
+                }
+
+                if (stub == city.DefaultTroop)
+                {
+                    return "Cant remove local troop";
+                }
+
+                procedure.TroopStubDelete(city, stub);
+            }
+
+            return "OK!";            
+        }
+
+        public string RemoveStructure(Session session, String[] parms)
+        {
+            bool help = false;
+            uint x = 0;
+            uint y = 0;
+
+            try
+            {
+                var p = new OptionSet
+                        {
+                                { "?|help|h", v => help = true }, 
+                                { "x=", v => x = uint.Parse(v.TrimMatchingQuotes()) },
+                                { "y=", v => y = uint.Parse(v.TrimMatchingQuotes()) }
+                        };
+                p.Parse(parms);
+            }
+            catch (Exception)
+            {
+                help = true;
+            }
+
+            if (help || x == 0 || y == 0)
+                return "removestructure --x=### --y=###";
+
+            Region region = World.Current.GetRegion(x, y);
+            if (region == null)
+                return "Invalid coordinates";
+
+            var structure = region.GetObjects(x, y).OfType<IStructure>().FirstOrDefault();
+            
+            if (structure == null)
+            {                
+                return "No structures found at specified coordinates";
+            }
+
+            using (Concurrency.Current.Lock(structure.City))
+            {
+                var removeAction = new StructureSelfDestroyPassiveAction(structure.City.Id, structure.ObjectId);
+                var result = structure.City.Worker.DoPassive(structure.City, removeAction, false);
+                
+                if (result != Error.Ok)
+                {
+                    return string.Format("Error: {0}", result);
+                }
+            }
+
+            return "OK!";
+        }
+
+        public string RenameCity(Session session, String[] parms)
+        {
+            bool help = false;
+            string cityName = string.Empty;
+            string newCityName = string.Empty;
+
+            try
+            {
+                var p = new OptionSet
+                        {
+                                { "?|help|h", v => help = true }, 
+                                { "city=", v => cityName = v.TrimMatchingQuotes() },
+                                { "newname=", v => newCityName = v.TrimMatchingQuotes() }
+                        };
+                p.Parse(parms);
+            }
+            catch (Exception)
+            {
+                help = true;
+            }
+
+            if (help || string.IsNullOrEmpty(cityName) || string.IsNullOrEmpty(newCityName))
+                return "renamecity --cityr=city --newname=name";
+
+            uint cityId;
+            if (!World.Current.FindCityId(cityName, out cityId))
+                return "City not found";
+
+            ICity city;
+            using (Concurrency.Current.Lock(cityId, out city))
+            {
+                if (city == null)
+                    return "City not found";
+
+                // Verify city name is valid
+                if (!City.IsNameValid(newCityName))
+                {
+                    return "City name is invalid";
+                }
+
+                lock (World.Current.Lock)
+                {
+                    // Verify city name is unique
+                    if (World.Current.CityNameTaken(newCityName))
+                    {
+                        return "City name is already taken";
+                    }
+
+                    city.BeginUpdate();
+                    city.Name = newCityName;
+                    city.EndUpdate();
+                }
+            }
+
+            return "OK!";
+        }
+    }
+}

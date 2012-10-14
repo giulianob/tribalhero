386a61abbd2ddfa91089281fb946a00ef4a4d1e4
anthony <anthony@localhost>
2012-10-14 14:50:08 -0400

    Added Victory point to tribe panel.

--- Game/Data/Tribe/ITribeFactory.cs
+++ Game/Data/Tribe/ITribeFactory.cs
@@ -2,7 +2,7 @@
 {
     public interface ITribeFactory
     {
-        ITribe CreateTribe(IPlayer owner, string name, string desc, byte level, int attackPoints, int defensePoints, Resource resource);
+        ITribe CreateTribe(IPlayer owner, string name, string desc, byte level, int victoryPoints, int attackPoints, int defensePoints, Resource resource);
 
         ITribe CreateTribe(IPlayer owner, string name);
     }

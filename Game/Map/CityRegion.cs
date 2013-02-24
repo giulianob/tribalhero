763bd720f28172a326574f146326167356fdf809
Giuliano Barberi <gbarberi@aotaonline.com>
2013-02-24 16:23:35 -0500

    Cleaning order of city region locks

--- Game/Map/CityRegion.cs
+++ Game/Map/CityRegion.cs
@@ -101,7 +101,7 @@ namespace Game.Map
                 ILockable[] playersInRegion;
                 lock (objLock)
                 {
-                    playersInRegion = data.Distinct().ToArray<ILockable>();
+                    playersInRegion = data.ToArray<ILockable>();
                 }
 
                 using (var lck = lockerFactory().Lock(playersInRegion))

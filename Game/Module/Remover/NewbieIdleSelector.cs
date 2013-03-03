92b41b3cdcb34f8b2a130b0408a9632fe9874432
Giuliano Barberi <gbarberi@aotaonline.com>
2013-03-03 09:53:19 -0500

    Increased aggressiveness of newbie idle selector to also remove players w/ 10 or less IP after 1 week

--- Game/Module/Remover/NewbieIdleSelector.cs
+++ Game/Module/Remover/NewbieIdleSelector.cs
@@ -12,20 +12,19 @@ namespace Game.Module.Remover
         {
             using (var reader = DbPersistance.Current.ReaderQuery(string.Format(@"
                                                                                 SELECT DISTINCT players.id player_id 
-                                                                                FROM   players 
-                                                                                        INNER JOIN cities 
-                                                                                            ON cities.player_id = players.id 
-                                                                                            AND cities.VALUE <= 5 
-                                                                                WHERE  players.last_login < DATE_SUB(Utc_timestamp(), INTERVAL 3 DAY) 
-                                                                                        AND players.online = 0 
-                                                                                        AND (SELECT COUNT(*) 
-                                                                                            FROM   cities c 
-                                                                                            WHERE  c.player_id = players.id 
-                                                                                            GROUP  BY c.player_id) = 1 
-                                                                                ORDER  BY players.id ASC "), new DbColumn
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             [
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ]
-            {}))
+                                                                                FROM players 
+                                                                                INNER JOIN cities ON cities.player_id = players.id 
+                                                                                WHERE  
+                                                                                    (
+                                                                                        (cities.VALUE <= 5 AND players.last_login < DATE_SUB(Utc_timestamp(), INTERVAL 3 DAY))
+                                                                                     OR (cities.VALUE <= 10 AND players.last_login < DATE_SUB(Utc_timestamp(), INTERVAL 7 DAY))
+                                                                                    )
+                                                                                    AND players.online = 0                                                                             
+                                                                                    AND (SELECT COUNT(*) 
+                                                                                         FROM cities c 
+                                                                                         WHERE c.player_id = players.id AND c.deleted = 0
+                                                                                         GROUP BY c.player_id) = 1 
+                                                                                ORDER BY players.id ASC"), new DbColumn[] {}))
             {
                 while (reader.Read())
                 {

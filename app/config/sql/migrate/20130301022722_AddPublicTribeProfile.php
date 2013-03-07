d0dd83200b158e3d7efcb84f80212ab9b60ffda3
Giuliano Barberi <gbarberi@aotaonline.com>
2013-03-06 19:52:08 -0500

    Merge with default

---
+++ app/config/sql/migrate/20130301022722_AddPublicTribeProfile.php
@@ -0,0 +1,12 @@
+<?php
+
+class AddPublicTribeProfile extends Ruckusing_BaseMigration {
+
+	public function up() {
+        $this->add_column("tribes", "public_desc", "text", array('null' => false, 'default' => ''));
+	}
+
+	public function down() {
+
+	}
+}

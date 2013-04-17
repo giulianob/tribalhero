d4902c525416641752151699ca5425044a65c64d
Giuliano Barberi <gbarberi@aotaonline.com>
2013-04-16 20:35:29 -0400

    Added leaving_tribesmate log

--- app/config/sql/migrate_up.bat
+++ app/config/sql/migrate_up.bat
@@ -1,13 +1,4 @@
 @ECHO OFF
 
-SET /P ANSWER=Migrate db to latest version (Y/N)?
-if /i {%ANSWER%}=={y} (goto :yes)
-if /i {%ANSWER%}=={yes} (goto :yes)
-goto :no
-
-:yes
 php ..\..\..\vendors\ruckusing\main.php db:migrate
 exit /b 0
-
-:no
-exit /b 1

0595adf83ce6903cf4224935dbd0fed25a987491
OscarMike <OscarMike@HTPC>
2013-05-25 17:48:05 -0400

    Increased IP requirement for creating new city.

--- Testing/FormulaTests/NewCityResourceTest.cs
+++ Testing/FormulaTests/NewCityResourceTest.cs
@@ -22,10 +22,10 @@ namespace Testing.FormulaTests
             influencePoints.Should().Be(100);
             wagons.Should().Be(50);
             formula.GetNewCityCost(10, out influencePoints, out wagons);
-            influencePoints.Should().Be(2800);
+            influencePoints.Should().Be(4600);
             wagons.Should().Be(500);
             formula.GetNewCityCost(30, out influencePoints, out wagons);
-            influencePoints.Should().Be(20400);
+            influencePoints.Should().Be(37800);
             wagons.Should().Be(1500);
         }
     }

8d692a5ad7172385d3b30f5466e58fbab2e3bb89
Giuliano Barberi <gbarberi@aotaonline.com>
2013-10-05 08:33:04 -0400

    Adding sound muted option

---
+++ app/config/sql/migrate/20130104022851_AddTroopInitialCountToStubs.php
@@ -0,0 +1,15 @@
+<?php
+
+class AddTroopInitialCountToStubs extends Ruckusing_BaseMigration {
+
+	public function up() {
+		$this->add_column("troop_stubs", "initial_count", 'smallinteger', array('length' => 5, 'unsigned' => true, 'null' => false, 'default' =>0) );
+		$this->add_column("troop_stubs", "attack_mode", 'boolean', array('null' => false, 'limit' => 3, 'unsigned' => true) );
+	}//up()
+
+	public function down() {
+		$this->remove_column("troop_stubs", "initial_count");
+		$this->remove_column("troop_stubs", "attack_mode");
+	}//down()
+}
+?>
---
+++ app/tests/fixtures/tribe_fixture.php
@@ -0,0 +1,26 @@
+<?php
+/* Tribe Fixture generated on: 2011-05-08 18:05:16 : 1304880256 */
+class TribeFixture extends CakeTestFixture {
+	var $name = 'Tribe';
+
+	var $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => '0', 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'desc' => array('type' => 'text', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'level' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 3),
+		'owner_id' => array('type' => 'integer', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	var $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'desc' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'level' => 1,
+			'owner_id' => 1
+		),
+	);
+}
+?>

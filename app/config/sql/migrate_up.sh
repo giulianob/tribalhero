d4841c5ada8ccf4a0f8df13cdc026e7092d643d1
Giuliano Barberi <gbarberi@aotaonline.com>
2013-12-27 11:56:42 -0500

    Removing check from upgrade script

--- app/config/sql/migrate_up.sh
+++ app/config/sql/migrate_up.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
-function ConfirmOrExit() {
-while true
-do
-echo -n "Please confirm (y or n) :"
-read CONFIRM
-case $CONFIRM in
-y|Y|YES|yes|Yes) break ;;
-n|N|no|NO|No)
-echo Aborting - you entered $CONFIRM
-exit
-;;
-*) echo Please enter only y or n
-esac
-done
-echo You entered $CONFIRM. Continuing ...
-}
 
-ConfirmOrExit
 php ../../../vendors/ruckusing/main.php db:migrate

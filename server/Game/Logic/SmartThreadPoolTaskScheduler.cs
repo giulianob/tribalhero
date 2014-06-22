64c562c0c9c2e889ffbe85d527685ad79fa3f33b
Giuliano Barberi <gbarberi@aotaonline.com>
2014-06-22 13:27:52 -0400

    Updating dependencies Adding smart thread pool support but disabled by default

---
+++ server/Game/Logic/SmartThreadPoolTaskScheduler.cs
@@ -0,0 +1,20 @@
+﻿using Amib.Threading;
+using Action = System.Action;
+
+namespace Game.Logic
+{
+    public class SmartThreadPoolTaskScheduler : ITaskScheduler
+    {
+        private readonly IWorkItemsGroup workItemsGroup;
+
+        public SmartThreadPoolTaskScheduler(IWorkItemsGroup workItemsGroup)
+        {
+            this.workItemsGroup = workItemsGroup;
+        }
+
+        public void QueueWorkItem(Action task)
+        {
+            workItemsGroup.QueueWorkItem(() => task(), WorkItemPriority.Normal);
+        }
+    }
+}

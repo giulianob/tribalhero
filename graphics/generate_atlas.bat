4b1b206ef5c685e705cc169163210649196120de
giulianob <gbarberi@aotaonline.com>
2014-09-06 19:18:18 -0400

    Moving scale to 1x for normal assets

--- graphics/generate_atlas.bat
+++ graphics/generate_atlas.bat
@@ -1,18 +1,16 @@
 set "PATH=C:/Program Files/CodeAndWeb/TexturePacker/bin;%PATH%"
-set "NO_SCALE=--variant 1:noscale"
-set "HD_SCALE_VARIANT=--variant 0.5:1x --variant 1:2x"
 
-set "DEFAULT_SETTINGS=--format sparrow --enable-rotation --multipack"
-set "DEFAULT_THEME_SETTINGS=%DEFAULT_SETTINGS% %NO_SCALE% --ignore-files *_BANNER.png --ignore-files *_WALL.png"
+set "DEFAULT_SETTINGS=--format sparrow --enable-rotation --multipack --variant 0.5:0.5x --variant 0.75:0.75x --variant 1:1x"
+set "DEFAULT_THEME_SETTINGS=%DEFAULT_SETTINGS% --ignore-files *_BANNER.png --ignore-files *_WALL.png"
 
 :: Tilemap
-TexturePacker.exe %DEFAULT_SETTINGS% %NO_SCALE%^
+TexturePacker.exe %DEFAULT_SETTINGS%^
   --data atlas\{v}\TILESET_ATLAS.xml^
   --sheet atlas\{v}\TILESET_ATLAS.png^
   TILESET.png
 
 :: Default Theme
-TexturePacker.exe %DEFAULT_SETTINGS% %NO_SCALE%^
+TexturePacker.exe %DEFAULT_SETTINGS%^
   --data atlas\{v}\DEFAULT_THEME_WALL_ATLAS.xml^
   --sheet atlas\{v}\DEFAULT_THEME_WALL_ATLAS.png^
   themes\default\DEFAULT_WALL.png
@@ -23,7 +21,7 @@ TexturePacker.exe %DEFAULT_THEME_SETTINGS%^
   themes\default
 
 :: Pirate Theme
-TexturePacker.exe %DEFAULT_SETTINGS% %NO_SCALE%^
+TexturePacker.exe %DEFAULT_SETTINGS%^
   --data atlas\{v}\PIRATES_THEME_WALL_ATLAS.xml^
   --sheet atlas\{v}\PIRATES_THEME_WALL_ATLAS.png^
   themes\pirates\PIRATES_WALL.png
@@ -34,7 +32,7 @@ TexturePacker.exe %DEFAULT_THEME_SETTINGS%^
   themes\pirates
 
 :: Feudal Japan Theme
-TexturePacker.exe %DEFAULT_SETTINGS% %NO_SCALE%^
+TexturePacker.exe %DEFAULT_SETTINGS%^
   --data atlas\{v}\FEUDALJAPAN_THEME_WALL_ATLAS.xml^
   --sheet atlas\{v}\FEUDALJAPAN_THEME_WALL_ATLAS.png^
   themes\feudaljapan\FEUDALJAPAN_WALL.png
@@ -45,13 +43,13 @@ TexturePacker.exe %DEFAULT_THEME_SETTINGS%^
   themes\feudaljapan
 
 :: Icons
-TexturePacker.exe %DEFAULT_SETTINGS% %HD_SCALE_VARIANT%^
+TexturePacker.exe %DEFAULT_SETTINGS%^
   --data atlas\{v}\ICONS_ATLAS.xml^
   --sheet atlas\{v}\ICONS_ATLAS.png^
   icons\general
 
 :: Misc Objects (e.g. forests), War Elephant Theme, and Cobblestone Road theme
-TexturePacker.exe %DEFAULT_SETTINGS% %NO_SCALE%^
+TexturePacker.exe %DEFAULT_SETTINGS%^
   --data atlas\{v}\OBJECTS_ATLAS.xml^
   --sheet atlas\{v}\OBJECTS_ATLAS.png^
   objects^
